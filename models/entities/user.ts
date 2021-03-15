a7b7857595946226205c03152ecc6bf38a14d08a
최은기 <galaxyhi4276@gmail.com>
2021-03-15 23:15:29 +0900

    기능: 사용자 엔티티와 레포지토리 작성

---
+++ models/entities/user.ts
@@ -0,0 +1,34 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+class User {
+  @PrimaryGeneratedColumn({ unsigned: true })
+  id!: number;
+
+  @Column({ nullable: false, length: 10 })
+  username!: string;
+
+  @Column({ nullable: false, length: 50 })
+  email!: string;
+
+  @Column({ nullable: false, length: 150 })
+  password!: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt!: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt!: Date;
+}
+
+export default User;
