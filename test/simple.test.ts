56ffccc9d01d34a0db1568a0843b0c39ae29728c
최은기 <galaxyhi4276@gmail.com>
2021-03-15 23:16:26 +0900

    lint: eslint 스타일 반영

--- test/simple.test.ts
+++ test/simple.test.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line import/extensions,import/no-unresolved
-import { returnTwo } from './testModule';
+import returnTwo from './testModule';
 
 describe('컨트롤러와 서비스 간단 테스트', () => {
   it('2 + 2는 4가 된다.', () => {
