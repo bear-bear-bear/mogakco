351508b0d64dd0b7a13e4324bdea34b9ac55e788
최은기 <galaxyhi4276@gmail.com>
2021-03-18 23:22:58 +0900

    테스트: 서비스 단 분리 및 사용자 조회 관련 테스트 케이스 작성

--- test/unit/Services/user.test.ts
+++ test/unit/Services/user.test.ts
@@ -3,18 +3,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import UserService from '../../services/user.service';
-import userDTO from '../../models/dto/userDTO';
-import User from '../../models/entities/user';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import userDTO from '../../../models/dto/userDTO';
+import UserService from '../../../services/user.service';
+import User from '../../../models/entities/user';
 
-let id = 1;
+let userId = 1;
 class UserMockRepository {
-  public async createUserOne(user: userDTO) {
+  public createUserOne(user: userDTO) {
+    if (!user.email || !user.username || !user.password) {
+      throw new BadRequestException('올바른 요청이 아닙니다.');
+    }
     return {
-      id,
+      id: userId,
       username: user.username,
       password: user.password,
       email: user.email,
+    };
+  }
+
+  public findUserOne(id: number) {
+    if (id === 999) {
+      throw new NotFoundException('존재하지 않는 요청입니다.');
+    }
+    return {
+      id: userId,
+      username: 'eunjunjung123',
+      email: 'bear-bear-bear@god.com',
     };
   }
 }
@@ -22,6 +37,7 @@ class UserMockRepository {
 describe('유저 CRUD 유닛 테스트', () => {
   let userService: UserService;
   let userRepository: Repository<User>;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -46,7 +62,7 @@ describe('유저 CRUD 유닛 테스트', () => {
       };
 
       const createdUser: User = await userService.createUserOne(user);
-      id++;
+      userId++;
 
       expect(createdUser.id).toBe(1);
       expect(createdUser).toMatchObject({ username: 'eunjunjung123' });
@@ -62,9 +78,39 @@ describe('유저 CRUD 유닛 테스트', () => {
 
       const createdUser: User = await userService.createUserOne(user);
       expect(createdUser.id).toBe(2);
+      userId = 1;
+    });
+
+    test('잘못된 사용자 생성은 수행되지 않는다.', async () => {
+      try {
+        await userService.createUserOne({
+          username: 'iloveyou',
+        } as userDTO);
+      } catch (e) {
+        expect(e).toBeInstanceOf(BadRequestException);
+        expect(e.message).toBe('올바른 요청이 아닙니다.');
+      }
     });
   });
-  // describe('유저 정보 조회', () => {});
+
+  describe('유저 정보 조회', () => {
+    test('id 1번 사용자 정보를 조회한다.', () => {
+      expect(userService.findUserOne(1)).toMatchObject({
+        id: userId,
+        username: 'eunjunjung123',
+        email: 'bear-bear-bear@god.com',
+      });
+    });
+
+    test('존재하지 않는 사용자를 조회한다.', () => {
+      try {
+        userService.findUserOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toBe('존재하지 않는 요청입니다.');
+      }
+    });
+  });
   // describe('유저 정보 수정', () => {});
   // describe('유저 정보 삭제', () => {});
 });
