bd12f76d10ef9b891c9266fd8cd26b7c6b60b48e
최은기 <galaxyhi4276@gmail.com>
2021-03-20 00:27:03 +0900

    refactor: 유저 서비스 목업 레포 -> helper 이동 ( 모듈화 )

---
+++ test/unit/helper/userMockRepository.ts
@@ -0,0 +1,65 @@
+import {
+  BadRequestException,
+  HttpStatus,
+  NotFoundException,
+} from '@nestjs/common';
+import userDTO from '../../../models/dto/userDTO';
+import updateUserRequest from '../Services/dto/updateUserRequest';
+
+class UserMockRepository {
+  public createUserOne(user: userDTO) {
+    if (!user.email || !user.username || !user.password) {
+      throw new BadRequestException('올바른 요청이 아닙니다.');
+    }
+    return {
+      id: 1,
+      username: user.username,
+      password: user.password,
+      email: user.email,
+    };
+  }
+
+  public testReadAndDelete(id: number, flag: boolean) {
+    /*
+      true is read therefore false is delete
+      sorry for design fucking method.
+      please lead me more better things JY & JJ.
+    */
+    if (id === 999) {
+      throw new NotFoundException('존재하지 않는 요청입니다.');
+    }
+    return flag
+      ? {
+          id: 1,
+          username: 'eunjunjung123',
+          email: 'bear-bear-bear@god.com',
+        }
+      : HttpStatus.ACCEPTED;
+  }
+
+  public findUserOne(id: number) {
+    return this.testReadAndDelete(id, true);
+  }
+
+  public deleteUser(id: number) {
+    return this.testReadAndDelete(id, false);
+  }
+
+  public updateUser(user: updateUserRequest) {
+    const currentUser = {
+      id: 1,
+      username: 'eunjunjung123',
+      email: 'bear-bear-bear@god.com',
+    };
+
+    if (Object.keys(user).includes('sex'))
+      throw new BadRequestException('올바른 요청이 아닙니다.');
+
+    return {
+      ...currentUser,
+      ...user,
+    };
+  }
+}
+
+export default UserMockRepository;
