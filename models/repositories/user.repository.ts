1aa299c5910a63b2944e7c8333bbd6a324d44dd6
galaxy4276 <galaxyhi4276@gmail.com>
2021-03-20 20:48:21 +0900

    사용자 유닛 테스트와 그에 근거한 유저 서비스 작성 (#13)
    * 테스트: 기존 가짜 테스트 목록과 함수 삭제
    
    * fix: soft delete column 강제 필수 여부 해제
    
    * 기능: 유닛 테스트에 맞춰 서비스 기능 수정
    
    * 테스트: 사용자 create 테스트 작성
    
    * fix: 프런트에 의한 설정 임시 제거
    
    * 테스트: eslint 가 테스트에 영향주는 부분 제거
    
    * 테스트: 서비스 단 분리 및 사용자 조회 관련 테스트 케이스 작성
    
    * 기능: TDD 에 근거한 간단 기능 작성
    
    * 테스트: 사용자 서비스 삭제 테스트 구현
    
    * 기능: TDD 에 근거한 사용사 삭제 기능 간단 구현
    
    * lint: eslint 에러 수정
    
    * refactor: 불필요한 async/await 제거
    
    * 기능: 유저 서비스 업데이트 적용 ( TDD 에 근거 )
    
    * 테스트: 유저 서비스 수정 DTO, 테스트 작성
    
    * refactor: 유저 서비스 목업 레포 -> helper 이동 ( 모듈화 )

--- models/repositories/user.repository.ts
+++ models/repositories/user.repository.ts
@@ -1,9 +1,32 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import User from '../entities/user';
+import userDTO from '../dto/userDTO';
+import updateUserRequest from '../../test/unit/Services/dto/updateUserRequest';
 
 @Injectable()
 @EntityRepository(User)
-class UserRepository extends Repository<User> {}
+class UserRepository extends Repository<User> {
+  public async createUserOne(user: userDTO): Promise<User> {
+    const newUser = new User();
+    newUser.password = user.password;
+    newUser.username = user.username;
+    newUser.email = user.email;
+    await this.save(newUser);
+    return newUser;
+  }
+
+  public findUserOne(id: number) {
+    return this.findOne(id);
+  }
+
+  public updateUser(user: updateUserRequest): Promise<User> {
+    return this.save(user);
+  }
+
+  public deleteUser(id: number) {
+    return this.softDelete(id);
+  }
+}
 
 export default UserRepository;
