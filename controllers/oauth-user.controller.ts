fc110894e17578963bee9daa350ef12ab4e664be
최은기 <galaxyhi4276@gmail.com>
2021-03-25 23:17:40 +0900

    lint: 린트 에러 해결

--- controllers/oauth-user.controller.ts
+++ controllers/oauth-user.controller.ts
@@ -3,8 +3,7 @@ import OauthUserService from '../services/oauth-user.service';
 
 @Controller('oauth')
 class OauthUserController {
-  constructor(private oauthService: OauthUserService) {
-  }
+  constructor(private oauthService: OauthUserService) {}
 
   async findOauthUserById(id: number) {
     const user = await this.oauthService.findOauthUserById(id);
