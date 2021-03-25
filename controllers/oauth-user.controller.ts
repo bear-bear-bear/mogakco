64bd9bc2289fc1c3f265e40c1d79ecda2cf05eb9
최은기 <galaxyhi4276@gmail.com>
2021-03-25 22:34:37 +0900

    feat: oauth 컨트롤러 선언

---
+++ controllers/oauth-user.controller.ts
@@ -0,0 +1,15 @@
+import { Controller } from '@nestjs/common';
+import OauthUserService from '../services/oauth-user.service';
+
+@Controller('oauth')
+class OauthUserController {
+  constructor(private oauthService: OauthUserService) {
+  }
+
+  async findOauthUserById(id: number) {
+    const user = await this.oauthService.findOauthUserById(id);
+    return user;
+  }
+}
+
+export default OauthUserController;
