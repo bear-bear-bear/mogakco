e333157f70b07714862c0f46f225a334b0f9beb2
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-04-01 21:16:32 +0900

    feat: svgr/webpack 라이브러리 설치 및 웹팩 설정
    * svg 파일을 컴포넌트처럼 사용할 수 있게 해주는 라이브러리입니다.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const path = require('path');
 module.exports = {
   webpack: config => {
     config.resolve.alias['~'] = path.resolve(__dirname);
+    config.module.rules.push({
+      // 웹팩설정에 로더 추가함
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ['@svgr/webpack'],
+    });
     return config;
   },
 };
