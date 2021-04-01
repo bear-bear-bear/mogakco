0be604da95918e5260b959f08c6c937be9b84981
최은기 <galaxyhi4276@gmail.com>
2021-04-02 01:37:29 +0900

    feat: 사용자와 사용자 인증 테이블 간 OneToOne Relation

--- models/entities/user_verifies.ts
+++ models/entities/user_verifies.ts
@@ -2,8 +2,11 @@ import { Entity } from 'typeorm';
 import {
   Column,
   CreateDateColumn,
+  JoinColumn,
+  OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm/browser';
+import Users from './users';
 
 @Entity({ name: 'user_verifies' })
 class UserVerifies {
@@ -18,6 +21,10 @@ class UserVerifies {
 
   @Column({ nullable: false, name: 'expired_at', length: 40 })
   expiredAt!: Date;
+
+  @OneToOne(() => Users)
+  @JoinColumn({ name: 'user_id' })
+  userId!: Users;
 }
 
 export default UserVerifies;
