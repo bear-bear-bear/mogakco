f5074e1b7363ce5927b208bd9b09f186247626c1
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-04-02 13:50:48 +0900

    feat: signup 페이지 header UI 추가

---
+++ components/SignUp/Header/index.js
@@ -0,0 +1,12 @@
+import signUpHeader from './style';
+import Logo from '~/public/assets/svg/logo.svg';
+
+const index = () => {
+  return (
+    <div css={signUpHeader}>
+      <Logo />
+    </div>
+  );
+};
+
+export default index;
---
+++ components/SignUp/Header/style.js
@@ -0,0 +1,10 @@
+import { css } from '@emotion/react';
+
+const signUpHeader = css`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 6.6875rem;
+`;
+
+export default signUpHeader;
