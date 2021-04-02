31d877018b17a7f27160832764f51e67bbf80cff
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-04-02 13:50:15 +0900

    feat: assets 디렉터리 alias 추가

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   webpack: config => {
     config.resolve.alias['~'] = path.resolve(__dirname);
+    config.resolve.alias.assets = path.resolve('public/assets');
     config.module.rules.push({
       // 웹팩설정에 로더 추가함
       test: /\.svg$/,
