46ff056cd32f62e91c90c7da81f1a9e680492c01
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-04-02 20:41:52 +0900

    feat: link 태그 적용

--- components/SignUp/Complete/index.js
+++ components/SignUp/Complete/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
-
+import Link from 'next/link';
 import {
   contentWrapperStyles,
   titleStyles,
   SubmitButton,
+  linkStyles,
 } from '../common/styles';
 
-const index = () => {
-  return (
-    <div css={contentWrapperStyles}>
-      <h1 css={titleStyles}>회원가입이 완료되었습니다!</h1>
+const index = () => (
+  <div css={contentWrapperStyles}>
+    <h1 css={titleStyles}>회원가입이 완료되었습니다!</h1>
+    <Link href="/" css={linkStyles}>
       <SubmitButton type="submit" complete>
-        모각코 참여하러 가기
+        <a>모각코 참여하러 가기</a>
       </SubmitButton>
-    </div>
-  );
-};
+    </Link>
+  </div>
+);
 
 export default index;
--- components/SignUp/Header/index.js
+++ components/SignUp/Header/index.js
@@ -1,10 +1,14 @@
+import Link from 'next/link';
 import signUpHeader from './style';
 import Logo from '~/public/assets/svg/logo.svg';
+import { linkStyles } from '../common/styles';
 
 const index = () => {
   return (
     <div css={signUpHeader}>
-      <Logo />
+      <Link href="/" css={linkStyles}>
+        <Logo style={{ cursor: 'pointer' }} />
+      </Link>
     </div>
   );
 };
