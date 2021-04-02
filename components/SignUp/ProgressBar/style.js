0a49790f268f37e20a699841b95d69c2380d8818
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-04-02 13:51:19 +0900

    feat: signup 페이지 ProgressBar UI 추가

---
+++ components/SignUp/ProgressBar/style.js
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react';
+
+export const wrapperStyles = css`
+  height: 0.625rem;
+  width: 100%;
+  background-color: #e0e0de;
+  border-radius: 0.3125rem;
+  margin-top: 3.75rem;
+`;
+
+export const fillerStyles = css`
+  height: 100%;
+  width: 0%;
+  background-color: #6a1b9a;
+  border-radius: inherit;
+  transition: 1s width ease-in-out;
+`;
