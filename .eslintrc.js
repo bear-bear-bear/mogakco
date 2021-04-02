df03eb8d232b646d093ab726875a55336375ef96
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-04-02 20:40:55 +0900

    feat: a 태그 유효 체크 린트 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -87,5 +87,13 @@ module.exports = {
         },
       },
     ],
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
   },
 };
