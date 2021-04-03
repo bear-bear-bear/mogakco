41dc6deae32604245f47d690d870799556c9b2d3
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-04-03 16:38:06 +0900

    feat: SignUp 컨테이너 추가

--- pages/signup.js
+++ pages/signup.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import SignUp from '~/components/SignUp';
+import SignUpContainer from '~/containers/SignUp/SignUpContainer';
 
 const signup = () => {
   return (
     <div>
-      <SignUp />
+      <SignUpContainer />
     </div>
   );
 };
