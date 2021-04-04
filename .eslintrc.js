99d1d8920a8be7b4e2cd5592c7d748569114fdb1
Kim June Jae <bear-bear-bear@kakao.com>
2021-04-04 19:26:51 +0900

    feat: export가 하나일땐 export default로 내보내야 하는 eslint 규칙 삭제 (#35)
    - emotion Styled 네이밍 컨벤션에 적합하지 않습니다

--- .eslintrc.js
+++ .eslintrc.js
@@ -72,5 +72,6 @@ module.exports = {
     'import/extensions': ['off'],
     'import/no-extraneous-dependencies': ['off'],
     'no-param-reassign': ['error', { props: false }],
+    'import/prefer-default-export': 'off',
   },
 };
