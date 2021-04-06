f2402c032fb0b424fe0ebc3687d24295a6e8d8ea
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-04-06 11:48:25 +0900

    feat: logo style svg 파일에서 직접 추가

--- components/SignUp/Header/index.js
+++ components/SignUp/Header/index.js
@@ -1,15 +1,14 @@
-import { useMemo } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import SignUpHeader from './style';
 import Logo from '~/public/assets/svg/logo.svg';
 import { LinkStyles } from '../common/styles';
 
 const Index = () => {
-  const pointer = useMemo(() => ({ cursor: 'pointer' }), []);
   return (
     <SignUpHeader>
       <Link href="/" css={LinkStyles}>
-        <Logo style={pointer} />
+        <Logo />
       </Link>
     </SignUpHeader>
   );
