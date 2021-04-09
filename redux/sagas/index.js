64c876b0c264fecd32d05edabb80da6c702a8ea1
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-04-09 22:10:32 +0900

    feat: info 사가 추가

--- redux/sagas/index.js
+++ redux/sagas/index.js
@@ -1,6 +1,7 @@
 import { all, fork } from 'redux-saga/effects';
-import { watchAuth } from './signup/auth';
+import watchAuth from './signup/auth';
+import watchInfo from './signup/info';
 
 export default function* rootSaga() {
-  yield all([fork(watchAuth)]);
+  yield all([fork(watchAuth), fork(watchInfo)]);
 }
