05687a6f083bf16e47825eb5e666df64c3393451
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-04-10 01:04:26 +0900

    refactor: 프론트 디렉터리 구조 변경 (#38)
    * feat: immer, saga 설치
    
    * refactor: 디렉터리 구조 변경
    
    containers -> 삭제
    example, EmoEx, CommonEx -> 삭제
    redux 관련 디렉터리(actions, reducers, store, selectors) -> redux 디렉터리로 통합
    
    * feat: 프론트 lint 수정

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,9 @@
 import React from 'react';
-import EmoEx from '~/components/example/EmoEx';
-// import CounterContainer from '../container/example/CounterContainer';
-import Counter from '~/components/example/Counter';
-// import TodosContainer from '../container/example/TodosContainer';
-import Todos from '~/components/example/Todos';
-import CommonExContainer from '~/containers/common/CommonExContainer';
 
 const App = () => {
   return (
     <div>
       <h1>Index page</h1>
-      <EmoEx />
-      {/* <CounterContainer /> */}
-      <Counter />
-      <hr />
-      <Todos />
-      {/* <TodosContainer /> */}
-      <hr />
-      <CommonExContainer />
     </div>
   );
 };
