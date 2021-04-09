81d65e906b9792cd0c3588d037b280aa62b4e39f
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-04-09 19:54:37 +0900

    feat: 컴포넌트 상태 추가

--- pages/signup.js
+++ pages/signup.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Header from '~/components/signup/header';
 import ProgressBar from '~/components/signup/progressBar';
 import Auth from '~/components/signup/auth';
@@ -6,13 +7,16 @@ import Info from '~/components/signup/info';
 import Interest from '~/components/signup/interest';
 import Complete from '~/components/signup/complete';
 import { SignUpPageContainer } from '../components/signup/common/styles';
+import { getVerifyEmailDone } from '~/redux/selectors/signup';
 
 const Signup = () => {
+  const verifyEmailDone = useSelector(getVerifyEmailDone);
+
   return (
     <SignUpPageContainer>
       <Header />
       <ProgressBar />
-      <Auth />
+      {!verifyEmailDone && <Auth />}
       <Info />
       <Interest />
       <Complete />
