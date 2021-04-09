ae95af62d60ab7a0dbf05e62d6ddcc399cb92bf0
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-04-09 22:11:32 +0900

    feat: info 컴포넌트 상태 추가

--- components/SignUp/Info/index.js
+++ components/SignUp/Info/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 
+import { useDispatch } from 'react-redux';
 import {
   Container,
   Title,
@@ -10,29 +11,126 @@ import {
   SubmitButton,
 } from '../common/styles';
 
+import { WarningText } from './style';
+
+import useInput from '~/hooks/useInput';
+import { verifyInfoRequest } from '~/redux/actions/signup/info';
+
 const Index = () => {
+  const dispatch = useDispatch();
+  const [username, onChangeUserName, setuserName] = useInput('');
+  const [password, onChangePassword, setPassword] = useInput('');
+  const [
+    passwordConfirm,
+    onChangePasswordConfirm,
+    setPasswordConfirm,
+  ] = useInput('');
+  const [initSubmit, setInitSubmit] = useState(false);
+  const [term, setTerm] = useState(false);
+  const [passwordMatchError, setPasswordMatchError] = useState(false);
+  const [termError, setTermError] = useState(false);
+
+  useEffect(() => {
+    // 처음 화면이 렌더링 됐을 땐 오류를 표시하지 않음
+    if (initSubmit) {
+      if (!term) {
+        setTermError(true);
+      } else {
+        setTermError(false);
+      }
+      if (password !== passwordConfirm) {
+        setPasswordMatchError(true);
+      } else {
+        setPasswordMatchError(false);
+      }
+    }
+  }, [password, passwordConfirm, term, initSubmit]);
+
+  const onChangeTerm = useCallback(() => {
+    setTerm(!term);
+  }, [term]);
+
+  const onSubmit = useCallback(
+    e => {
+      e.preventDefault();
+      setInitSubmit(true);
+      if (password !== passwordConfirm) {
+        return setPasswordMatchError(true);
+      }
+      if (!term) {
+        return setTermError(true);
+      }
+      dispatch(verifyInfoRequest(username, password));
+      setuserName('');
+      setPassword('');
+      setPasswordConfirm('');
+    },
+    [
+      dispatch,
+      username,
+      password,
+      passwordConfirm,
+      term,
+      setuserName,
+      setPassword,
+      setPasswordConfirm,
+    ],
+  );
+
   return (
     <Container>
-      <Title>이름과 비밀번호를 입력하세요</Title>
-      <Form action="">
+      <Title>별명과 비밀번호를 입력하세요</Title>
+      <Form action="" onSubmit={onSubmit}>
         <InputWrapper>
-          <Label htmlFor="name">이름</Label>
-          <Input type="text" id="name" page="info" required />
+          <Label htmlFor="username">별명</Label>
+          <Input
+            type="text"
+            id="username"
+            value={username}
+            onChange={onChangeUserName}
+            page="info"
+            required
+          />
         </InputWrapper>
         <InputWrapper>
           <Label htmlFor="password">비밀번호</Label>
-          <Input type="password" id="password" page="info" required />
+          <Input
+            type="password"
+            id="password"
+            value={password}
+            onChange={onChangePassword}
+            page="info"
+            required
+          />
         </InputWrapper>
         <InputWrapper>
           <Label htmlFor="passwordConfirm">비밀번호 확인</Label>
-          <Input type="password" id="passwordConfirm" page="info" required />
+          <Input
+            type="password"
+            id="passwordConfirm"
+            value={passwordConfirm}
+            onChange={onChangePasswordConfirm}
+            page="info"
+            required
+          />
         </InputWrapper>
+        <WarningText>※ 비밀번호는 8자리 이상 입력하셔야 합니다</WarningText>
+        {passwordMatchError && (
+          <WarningText>비밀번호가 일치하지 않습니다!</WarningText>
+        )}
         <InputWrapper page="info">
-          <Input type="checkbox" id="policy" page="info" required width="" />
+          <Input
+            type="checkbox"
+            id="policy"
+            page="info"
+            value={term}
+            onChange={onChangeTerm}
+          />
           <Label htmlFor="policy" page="info">
             (필수)개인정보 수집 및 이용에 동의하겠습니다.
           </Label>
         </InputWrapper>
+        {termError && <WarningText>약관에 동의하셔야 합니다!</WarningText>}
         <SubmitButton type="submit" complete={false}>
           계속
         </SubmitButton>
@@ -41,4 +139,4 @@ const Index = () => {
   );
 };
 
-export default Index;
+export default React.memo(Index);
