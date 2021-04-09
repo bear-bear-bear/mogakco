eb6950238fecb929ca093e533ee1d1586eb8e4d7
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-04-09 14:14:57 +0900

    feat: redux 디렉터리 alias 설정

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
   webpack: config => {
     config.resolve.alias['~'] = path.resolve(__dirname);
     config.resolve.alias.assets = path.resolve('public/assets');
+    config.resolve.alias.redux = path.resolve('redux', __dirname);
     config.module.rules.push({
       // 웹팩설정에 로더 추가함
       test: /\.svg$/,
