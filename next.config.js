a7de1fa23d6bb899790d4ea2e7d56adb97db157f
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-04-09 16:13:41 +0900

    remove: redux alias 삭제(불필요)

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ module.exports = {
   webpack: config => {
     config.resolve.alias['~'] = path.resolve(__dirname);
     config.resolve.alias.assets = path.resolve('public/assets');
-    config.resolve.alias.redux = path.resolve('redux', __dirname);
     config.module.rules.push({
       // 웹팩설정에 로더 추가함
       test: /\.svg$/,
