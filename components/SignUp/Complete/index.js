9f3c315a0a00a50aa427ba6344f3b12401f17d19
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-04-10 14:35:43 +0900

    refactor: 리다이렉션 버튼을 Link에서 Router.push로 변경
    * 하이퍼링크에는 Link 태그
    * 리다이렉션은 Router.push()

--- components/SignUp/Complete/index.js
+++ components/SignUp/Complete/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import Link from 'next/link';
+import React, { useCallback } from 'react';
+import Router from 'next/router';
 import { Container, Title, SubmitButton, LinkStyles } from '../common/styles';
 
-const Index = () => (
-  <Container>
-    <Title>회원가입이 완료되었습니다!</Title>
-    <Link href="/" css={LinkStyles}>
-      <SubmitButton type="submit" complete>
+const Index = () => {
+  const onClickLogo = useCallback(() => {
+    Router.push('/');
+  }, []);
+  return (
+    <Container css={LinkStyles}>
+      <Title>회원가입이 완료되었습니다!</Title>
+      <SubmitButton type="button" onClick={onClickLogo} complete>
         <a>모각코 참여하러 가기</a>
       </SubmitButton>
-    </Link>
-  </Container>
-);
+    </Container>
+  );
+};
 
 export default Index;
