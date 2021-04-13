3da5685a04f046a6bdf146cddb08325b8d421d27
최은기 <galaxyhi4276@gmail.com>
2021-04-13 23:28:45 +0900

    feat: 이메일 기능 (1)

--- modules/mail.module.ts
+++ modules/mail.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+import { join } from 'path';
 
 @Module({
   imports: [
     MailerModule.forRoot({
-      transport: '',
-      defaults: {
-        from: '',
-      },
       template: {
-        dir: __dirname,
+        dir: join(__dirname, '../services/email'),
         adapter: new PugAdapter(),
       },
     }),
--- services/email.service.ts
+++ services/email.service.ts
@@ -1,7 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
+import { emailSucessLog } from '../lib/backend/dev';
+
+interface UserVerifyEmailDTO {
+  to: string;
+  username: string;
+  hash: string;
+}
 
 @Injectable()
 export default class EmailService {
   constructor(private readonly mailerService: MailerService) {}
+
+  public userVerify({ to, username, hash }: UserVerifyEmailDTO): void {
+    this.mailerService
+      .sendMail({
+        to,
+        from: process.env.EMAIL_ADMIN,
+        subject: 'Mogakco forwards Autentication to your email 🥰',
+        template: 'user-verify',
+        context: {
+          username,
+          to,
+          hash,
+        },
+      })
+      .then(email => emailSucessLog(email));
+  }
 }
