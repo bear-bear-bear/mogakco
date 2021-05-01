ff86e10d02d836107d86990c331e0e6fd8159e30
최은기 <galaxyhi4276@gmail.com>
2021-05-01 13:52:53 +0900

    feature: 에러 사항 추가

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -72,6 +72,11 @@ class AuthController {
     };
   }
 
+  /**
+   * @desc 회원가입 컨트롤러 입니다.
+   * @returns 성공적으로 회원가입 된 사용자 객체
+   * @author galaxy4276
+   */
   @Post()
   async join(@Body() user: createUserDTO): Promise<response> {
     const res = await this.userService.join(user);
@@ -83,6 +88,9 @@ class AuthController {
    */
   @Post('/prepare')
   async prepareJoin(@Body('email') email: string) {
+    if (!email) throw Error('이메일 정보가 존재하지 않습니다.');
+
+    console.log({ email });
     try {
       const [verifyToken, to] = await this.userService.prepareJoin(email);
       this.emailService.userVerify({
