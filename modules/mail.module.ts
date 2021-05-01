c1b2e3c643c1ab55425ee42182d33fcd5564c4f2
최은기 <galaxyhi4276@gmail.com>
2021-05-01 13:52:08 +0900

    기능: 이메일 설정 추가 & 모듈에 설정 값 설명 추가

--- modules/mail.module.ts
+++ modules/mail.module.ts
@@ -4,15 +4,23 @@ import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 import { join } from 'path';
 import { ConfigModule } from '@nestjs/config';
 
+/**
+ * @description MailerModule 에 어떠한 모듈을 설정해야 하는 지 기술함.
+ * service: 이메일 공급자 서비스 이름
+ * host: 이메일 호스트 도메인
+ * port: 이메일 서비스 포트
+ * auth->user: 이메일 계정
+ * auth->pass: 이메일 비밀번호
+ */
 @Module({
   imports: [
     MailerModule.forRootAsync({
       imports: [ConfigModule.forRoot()],
       useFactory: async () => ({
         transport: {
-          service: 'gmail',
-          host: 'smtp.gmail.com',
-          port: 587,
+          service: process.env.EMAIL_SERVICE_NAME as string,
+          host: process.env.EMAIL_HOST as string,
+          port: parseInt(process.env.EMAIL_SERVICE_PORT as string, 10),
           secure: process.env.NODE_ENV === 'production',
           auth: {
             type: 'login',
