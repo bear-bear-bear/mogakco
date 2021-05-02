2e4fe6fa35324dce3b5e84486f88501c4c021a56
최은기 <galaxyhi4276@gmail.com>
2021-05-03 03:31:37 +0900

    feature: 시범용 테스트 컴포넌트 작성

---
+++ test/unit/frontend/pages/TestRenderer.test.jsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import TestRenderer from '../components/TestRenderer';
+
+describe('리액트 테스트 케이스', () => {
+  test('렌더링 테스트', () => {
+    render(<TestRenderer />);
+    screen.getByText('여행기분떡상하기');
+  });
+});
