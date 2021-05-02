86c02a88285f55f7cb4716458bb90fff6b0d8588
bearbearbear <bear-bear-bear@kakao.com>
2021-05-03 00:59:30 +0900

    feat: 유저 로그인, 로그아웃 액션에 대한 초기상태, 리듀서 작성 * 더미 유저 적용

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import signUpReducer from './SignUp';
+import user from './common/user';
 
-const rootReducer = combineReducers({ signUpReducer });
+const rootReducer = combineReducers({
+  user,
+  signUpReducer,
+});
 
 export default rootReducer;
