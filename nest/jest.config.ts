94eb215349e8f62b72b19641c55347a1baea376a
최은기 <galaxyhi4276@gmail.com>
2021-05-30 04:00:44 +0900

    feature: unit-test script 에서 ts 로 관리 및 path alias 지정

--- nest/jest.config.ts
+++ nest/jest.config.ts
@@ -1,14 +1,18 @@
 import { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
-  moduleFileExtensions: ['ts', 'json', 'js', 'jsx'],
   rootDir: './',
+  moduleFileExtensions: ['ts', 'js', 'json'],
   testEnvironment: 'node',
-  collectCoverageFrom: ['**/*.(t|j)s'],
-  transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
-    '^.+\\.svg$': '<rootDir>/lib/svgTransform.js',
+  testRegex: '.test.ts$',
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '^@controllers/(./*)$': '<rootDir>/controllers/$1',
+    '^@lib/(./*)$': '<rootDir>/lib$1',
+    '^@models/(./*)$': '<rootDir>/models$1',
+    '^@modules/(./*)$': '<rootDir>/modules$1',
+    '^@services/(./*)$': '<rootDir>/services$1',
+    '^@test/(./*)$': '<rootDir>/test$1',
   },
 };
 
