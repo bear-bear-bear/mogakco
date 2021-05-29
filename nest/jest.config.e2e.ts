9ed8230b7c8c1d69593e1a3057391d4217b22602
최은기 <galaxyhi4276@gmail.com>
2021-05-30 04:00:20 +0900

    feature: 기존 e2e script 에서 e2e.ts 로 테스트 관리 작성

---
+++ nest/jest.config.e2e.ts
@@ -0,0 +1,39 @@
+import { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  rootDir: './',
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testEnvironment: 'node',
+  testRegex: '.e2e.test.ts$',
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '^@controllers/(./*)$': '<rootDir>/controllers/$1',
+    '^@lib/(./*)$': '<rootDir>/lib$1',
+    '^@models/(./*)$': '<rootDir>/models$1',
+    '^@modules/(./*)$': '<rootDir>/modules$1',
+    '^@services/(./*)$': '<rootDir>/services$1',
+    '^@test/(./*)$': '<rootDir>/test$1',
+  },
+};
+
+export default config;
+
+// TODO!: 아래 옵션기반으로 설정 보완 필요
+// "jest": {
+//   "moduleFileExtensions": [
+//     "js",
+//     "json",
+//     "ts"
+//   ],
+//     "rootDir": "./",
+//     "testRegex": ".*\\.spec\\.ts$",
+//     "transform": {
+//     "^.+\\.(t|j)s$": "ts-jest"
+//   },
+//   "collectCoverageFrom": [
+//     "**/*.(t|j)s"
+//   ],
+//     "coverageDirectory": "../coverage",
+//     "testEnvironment": "node"
+// }
+//
