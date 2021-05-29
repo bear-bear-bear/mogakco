63183db9b4d57f17bd5d6ae8d373d71d8fea8f63
galaxy4276 <galaxyhi4276@gmail.com>
2021-05-29 18:15:31 +0900

    기능: 백엔드 마이그레이션 추가 및 CI 데이터베이스 업데이트 (#81)
    * fix: package-lock.json 오류 수정
    
    * feature: gitignore build 파일 ignore 처리
    
    * feature: .env 추가 필요 값 작성
    
    * feature: lib 추가, migration 전용 명령어 작성
    
    * feature: 어플리케이션 마이그레이션 경로 지정
    
    * 기능: 마이그레이션 업데이트
    
    * fix: ci 용 명령어 추가
    
    * fix: CI 순서 변경

---
+++ nest/migrations/1622256071819-user.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class user1622256071819 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'users',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'bigint',
+                    unsigned: true,
+                    isPrimary: true,
+                },
+                {
+                    name: 'username',
+                    isNullable: false,
+                    type: 'varchar',
+                    length: '10',
+                },
+                {
+                    name: 'created_at',
+                    type: 'timestamp',
+                    default: 'now()',
+                },
+                {
+                    name: 'updated_at',
+                    type: 'timestamp',
+                    default: 'now()',
+                },
+                {
+                    name: 'deleted_at',
+                    type: 'timestamp',
+                }
+            ]
+        }), true);
+    }
+
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
+    }
+}
