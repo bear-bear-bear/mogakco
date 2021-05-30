f19a7f7ea8d3509fbf493c6e97d2bdbf2db6512c
최은기 <galaxyhi4276@gmail.com>
2021-05-30 19:22:13 +0900

    feature: TIMESTAMP 정보 작성

--- nest/models/entities/user-field.entity.ts
+++ nest/models/entities/user-field.entity.ts
@@ -1,4 +1,11 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity({
   name: 'users-field',
@@ -9,6 +16,12 @@ class UserFieldEntity extends BaseEntity {
 
   @Column({ length: 20, nullable: false })
   public name!: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt!: Date;
 }
 
 export default UserFieldEntity;
