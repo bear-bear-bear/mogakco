cc3e6f8feb182b28179c5b235c24aa3ab3b0d16a
bearbearbear <bear-bear-bear@kakao.com>
2021-05-31 02:35:15 +0900

    fix: 서버 측 API 변경에 따라 API url 변경, 각 API에 대한 주석 추가

--- next/lib/APIs.js
+++ next/lib/APIs.js
@@ -1,5 +1,4 @@
 import apiClient from './apiClient';
-
 /**
  * @desc redux/sagas 디렉터리 구성 기준으로 작성됩니다.
  * 추후 API 개수가 많아지고 파일 크기가 늘어나면 디렉터리 단위로 분리 고려.
@@ -7,17 +6,24 @@ import apiClient from './apiClient';
 
 // common/user
 export const userAPIs = {
-  logInAPI: (data) => apiClient.post('/user/login', data),
-  logOutAPI: () => apiClient.get('/user/logout'),
+  // 로그인
+  logInAPI: (data) => apiClient.post('/api/auth/login', data),
+  // 로그아웃
+  logOutAPI: () => apiClient.get('/api/auth/logout'),
 };
 
 // signup
 export const signupAPIs = {
+  // 검증 이메일 전송
   sendEmailAPI: (email) =>
-    apiClient.post('/api/user/send-token/before-register', { email }),
+    apiClient.post('/api/auth/send-token/before-register', { email }),
+  // 이메일 검증에 성공시 나타나는 쿼리의 이메일에 대해 다시 한 번 검증
   verifyEmailAPI: (email) =>
-    apiClient.get(`/api/user/is-verified/before-register?email=${email}`),
+    apiClient.get(`/api/auth/is-verified/before-register?email=${email}`),
+  // 개발 분야 목록 불러오기
   loadSkillsAPI: () => apiClient.get(`/api/user/skills`),
+  // 직업 목록 불러오기
   loadJobsAPI: () => apiClient.get(`/api/user/jobs`),
-  signUpApI: (data) => apiClient.post('/api/user', data),
+  // 회원가입
+  signUpApI: (data) => apiClient.post('/api/auth', data),
 };
