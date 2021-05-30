b8e0d56778a3d4c58095fbe447e4f649f5fa2fcb
최은기 <galaxyhi4276@gmail.com>
2021-05-30 19:52:23 +0900

    fix: api 변경과 함께 의존성있던 설정들 변경

--- nest/controllers/user.controller.ts
+++ nest/controllers/user.controller.ts
@@ -1,7 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('user')
 class UserController {
+  @Get('/test')
   test() {
     return 'test';
   }
