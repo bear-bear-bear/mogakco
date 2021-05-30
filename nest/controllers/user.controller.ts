3ea882a23dfe1dd825fbc42320be5c726b5e8073
최은기 <galaxyhi4276@gmail.com>
2021-05-30 20:55:59 +0900

    feature: 분야 목록 불러오기 컨트롤러 작성

---
+++ nest/controllers/user.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get } from '@nestjs/common';
+import UserService from '@services/user.service';
+
+@Controller('user')
+class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get('/test')
+  test() {
+    return 'test';
+  }
+
+  @Get('/skills')
+  async getAllFields() {
+    const fieldList = await this.userService.findAllFields();
+    return fieldList;
+  }
+}
+
+export default UserController;
