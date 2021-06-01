15f1502e3d49ce6868c49ba49d17f2accdaa65d2
최은기 <galaxyhi4276@gmail.com>
2021-06-01 14:48:19 +0900

    feature: 클래스 상속을 위한 추상 클래스 작성

---
+++ nest/models/entities/helper/abstract.ts
@@ -0,0 +1,40 @@
+import {
+  BaseEntity,
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export abstract class BaseEntitySoftDelete extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt!: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt!: Date;
+}
+
+export abstract class BaseEntityHardDelete extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt!: Date;
+}
+
+export abstract class BaseEntityOnlyCreatedAt extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: Date;
+}
