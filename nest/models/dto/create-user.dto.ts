44b70c775f538b60afd8f40e27b6b79387bca33a
최은기 <galaxyhi4276@gmail.com>
2021-06-02 15:51:33 +0900

    feature: Issue 란 정규식 요청 반영

--- nest/models/dto/create-user.dto.ts
+++ nest/models/dto/create-user.dto.ts
@@ -1,18 +1,49 @@
-import { IsEmail, IsString, MaxLength, MinLength, Matches } from 'class-validator';
+import {
+  IsEmail,
+  IsString,
+  MaxLength,
+  MinLength,
+  Matches,
+  IsNotEmpty,
+  IsArray,
+  ArrayMaxSize,
+  IsNumber,
+} from 'class-validator';
+
+const passwordRegex = new RegExp(
+  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@!%*#?&])[A-Za-z\d$@!%*#?&]{8,50}$/,
+);
+const nickNameRegex = new RegExp(/[가-힣a-zA-Z0-9.]{1,12}$/);
+const emptyMessage = '빈 값이 올 수 없습니다.';
 
 class CreateUserDto {
-  @MinLength(5, { message: '닉네임은 최소 3글자 이상이어야 합니다.' })
-  @MaxLength(10, { message: '닉네임은 10글자를 넘을 수 없습니다.' })
+  @IsNotEmpty({ message: emptyMessage })
   @IsString()
+  @MinLength(1, { message: '닉네임은 최소 1글자 이상이어야 합니다.' })
+  @MaxLength(12, { message: '닉네임은 12글자를 넘을 수 없습니다.' })
+  @Matches(nickNameRegex, { message: '닉네임 형식이 맞지 않습니다.' })
   readonly username!: string;
 
+  @IsNotEmpty({ message: emptyMessage })
+  @IsString()
   @IsEmail()
   @MaxLength(50, { message: '이메일은 50글자를 넘을 수 없습니다.' })
   readonly email!: string;
 
+  @IsNotEmpty({ message: emptyMessage })
   @IsString()
-  @Matches('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$')
+  @MaxLength(15, { message: '패스워드는 15자 이상을 넘을 수 없습니다.' })
+  @Matches(passwordRegex, { message: '패스워드 형식이 맞지 않습니다.' })
   readonly password!: string;
+
+  @IsNotEmpty({ message: emptyMessage })
+  @IsArray({ message: '관심 분야는 배열 형태여야 합니다.' })
+  @ArrayMaxSize(5, { message: '관심 분야는 5개 이상 등록 할 수 없습니다.' })
+  skills!: number[];
+
+  @IsNotEmpty({ message: emptyMessage })
+  @IsNumber()
+  job!: number;
 }
 
 export default CreateUserDto;
