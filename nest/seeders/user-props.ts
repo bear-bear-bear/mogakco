166b2436c5567c24a3d61f33a7e7f17ad39d2748
최은기 <galaxyhi4276@gmail.com>
2021-06-04 15:41:24 +0900

    feature: 시더 테스트 계정 추가

--- nest/seeders/user-props.ts
+++ nest/seeders/user-props.ts
@@ -1,15 +1,24 @@
 import { Factory, Seeder } from 'typeorm-seeding';
+import { getCustomRepository } from 'typeorm';
 import makeHash from '../lib/makeHash';
 import UserJobEntity from '../models/entities/users-job.entity';
 import { jobDataLength } from '../factory/users-job.factory';
 import { fieldDataLength } from '../factory/users-field.factory';
 import UserFieldEntity from '../models/entities/user-field.entity';
 import UserEntity from '../models/entities/user.entity';
+import UserRepository from '../models/repositories/user.repository';
 
 export default class CreateUserJobs implements Seeder {
   async run(factory: Factory) {
     await factory(UserJobEntity)().createMany(jobDataLength);
     await factory(UserFieldEntity)().createMany(fieldDataLength);
+    await getCustomRepository(UserRepository).createUserOne({
+      username: 'mogauser',
+      password: await makeHash('mogapass'),
+      email: process.env.EMAIL_ADMIN as string,
+      skills: [1, 2, 3],
+      job: 1,
+    });
     await factory(UserEntity)()
       .map(async ({ username, email, password: plain }: UserEntity): Promise<UserEntity> => {
         const password = await makeHash(plain);
