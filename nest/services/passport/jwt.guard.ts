286de533c0b75eca09d085df2bb36d878e2d3eb3
최은기 <galaxyhi4276@gmail.com>
2021-06-09 22:16:03 +0900

    remove: 안 쓰는 가드 삭제

--- nest/services/passport/jwt.guard.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-@Injectable()
-class JwtAuthGuard extends AuthGuard('jwt') {}
-export default JwtAuthGuard;
