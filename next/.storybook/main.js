205213f11430ccd7a17d3a4a7f31ffbdf381c5f4
최은기 <galaxyhi4276@gmail.com>
2021-06-15 15:01:28 +0900

    feature: storybook webpack config emotion 충돌 해결

--- next/.storybook/main.js
+++ next/.storybook/main.js
@@ -1,7 +1,22 @@
+const { join } = require('path');
+
+const resolvePath = (path) => join(process.cwd(), path);
+
 module.exports = {
+  webpackFinal: async (config) => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        '@emotion/styled': resolvePath('node_modules/@emotion/styled'),
+      },
+    },
+  }),
   "stories": [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../components/common/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
