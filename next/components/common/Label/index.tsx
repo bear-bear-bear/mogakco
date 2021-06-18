33b0d5dc17e30c7b8ce171ec8231a52aed0fe555
최은기 <galaxyhi4276@gmail.com>
2021-06-18 18:33:41 +0900

    request: request 해결 및 request 생성

--- next/components/common/Label/index.tsx
+++ next/components/common/Label/index.tsx
@@ -4,7 +4,7 @@ import * as S from './style';
 
 /**
  * @desc label의 기본 스타일을 정의합니다. common/TextInput 와 함께 사용한다면 size 값을 맞춰주세요.
- * direction은 정의할 input의 상대적인 방향을 말합니다. 해당 방향으로 margin  이 적용됩니다.
+ * direction은 정의할 input의 상대적인 방향을 말합니다. 해당 방향으로 margin 이 적용됩니다.
  */
 
 interface Props extends LabelHTMLAttributes<HTMLLabelElement> {
