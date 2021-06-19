3cb897dfeee710ec4361c43c012b601cb3d93426
최은기 <galaxyhi4276@gmail.com>
2021-06-19 19:39:10 +0900

    feature: InputWrapper 타입 작업

--- next/components/common/InputWrapper/index.tsx
+++ next/components/common/InputWrapper/index.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { FC, HTMLAttributes } from 'react';
 
 import * as S from './style';
 
 /**
- * @desc label과 input을 묶기 위한 inputWrapper
+ * @desc label 과 input 을 묶기 위한 inputWrapper
  * flex-direction: column; 입니다.
  */
-const InputWrapper = ({ children, ...rest }) => (
-  <S.InputWrapper {...rest}>{children}</S.InputWrapper>
-);
+const InputWrapper: FC<HTMLAttributes<HTMLDivElement>> = ({
+  children,
+  ...rest
+}) => <S.InputWrapper {...rest}>{children}</S.InputWrapper>;
 
 export default InputWrapper;
