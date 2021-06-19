ba598c0e1c2a8015ec11d15f34d2a7bcd53372ad
최은기 <galaxyhi4276@gmail.com>
2021-06-19 19:40:07 +0900

    feature: Warning 타입 작업

--- next/components/common/Warning/index.tsx
+++ next/components/common/Warning/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { FC, HTMLAttributes } from 'react';
 
 import * as S from './style';
 
-const Warning = ({ children, ...rest }) => (
-  <S.Warning {...rest}>{children}</S.Warning>
-);
+const Warning: FC<HTMLAttributes<HTMLParagraphElement>> = ({
+  children,
+  ...rest
+}) => <S.Warning {...rest}>{children}</S.Warning>;
 
 export default Warning;
