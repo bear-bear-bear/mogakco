5a949885757ddb6305a2ee0f4688b4f54c3d55ca
galaxy4276 <galaxyhi4276@gmail.com>
2021-06-23 02:21:22 +0900

    FEATURE: 프런트엔드 Path Alias 수정 및 추가 (#177)
    * modify: next.js docs 참고 후 수정
    
    * feature: alias 추가
    
    * modify: 수정된 path alias 반영
    
    * modify: path 모듈 제거 (unused)

--- next/next.config.js
+++ next/next.config.js
@@ -1,13 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { resolve } = require('path');
-
 /**
  * @type {import('next/dist/next-server/server/config').NextConfig}
  * */
 module.exports = {
   webpack: (config) => {
-    config.resolve.alias['~'] = resolve(__dirname);
-    config.resolve.alias.assets = resolve(__dirname, './public/assets');
     config.module.rules.push({
       // 웹팩설정에 로더 추가함
       test: /\.svg$/,
