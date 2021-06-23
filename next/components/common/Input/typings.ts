26d913f898ec5743de075a5ffe8c920b56d6e30b
최은기 <galaxyhi4276@gmail.com>
2021-06-23 15:24:44 +0900

    fix: 타입 오버라이딩 수정

--- next/components/common/Input/typings.ts
+++ next/components/common/Input/typings.ts
@@ -1,14 +1,18 @@
-import React, { Dispatch, SetStateAction, ChangeEvent } from 'react';
+import React, {
+  Dispatch,
+  SetStateAction,
+  ChangeEvent,
+  InputHTMLAttributes,
+} from 'react';
 
 // FIXME: HTMLInputElement를 extend 할 시 emotion 컴포넌트에 의해 거부됨.. 사용한 html 속성을 다 직접 정의해야 할 것 같은데 다른 방법은 없을지 ㅠ
-export interface IDefaultProps {
+export interface IDefaultProps extends InputHTMLAttributes<HTMLInputElement> {
   type: 'checkbox' | 'text' | 'password' | 'email';
   scale?: 'small' | 'medium';
   name?: string;
   id?: string;
   disabled?: boolean;
   required?: boolean;
-  spellCheck?: string;
   placeholder?: string;
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
   onFocus?: (e: ChangeEvent<HTMLInputElement>) => void;
