b45147dfc776a1035bec588058ef55840a646750
최은기 <galaxyhi4276@gmail.com>
2021-06-30 01:54:43 +0900

    feature: Chat Service 분리

--- nest/modules/chat.module.ts
+++ nest/modules/chat.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import RoomRepository from '@models/repositories/room.repository';
 import ChatController from '@controllers/chat.controller';
+import ChatGateway from '@controllers/chat.gateway';
+import ChatService from '@services/chat.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([RoomRepository])],
   controllers: [ChatController],
+  providers: [ChatGateway, ChatService],
 })
 class ChatModule {}
 
