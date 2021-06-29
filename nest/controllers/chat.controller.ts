08b264961f7e70703ecc7c63ea8411cce8928e66
최은기 <galaxyhi4276@gmail.com>
2021-06-30 01:54:28 +0900

    feature: chat 검증 api 안정성 향상 및 구조 변경

--- nest/controllers/chat.controller.ts
+++ nest/controllers/chat.controller.ts
@@ -1,27 +1,23 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
-import RoomRepository from '@models/repositories/room.repository';
+import { BadRequestException, Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import ChatService from '@services/chat.service';
 
 @Controller('chat')
 class ChatController {
-  constructor(private roomRepository: RoomRepository) {}
+  constructor(private readonly chatService: ChatService) {}
 
   @Get('/available/:id')
   async isAvailableChatRoom(
     @Param('id', ParseIntPipe)
     roomId: number,
   ) {
-    const isRoom = await this.roomRepository.findOne(roomId);
-    if (isRoom !== undefined) {
+    const available = await this.chatService.isAvailable(roomId);
+    if (available) {
       return {
-        message: true,
+        message: '채팅방이 존재합니다',
         statusCode: 200,
       };
     }
-
-    return {
-      message: false,
-      statusCode: 200,
-    };
+    throw new BadRequestException('채팅방이 존재하지 않습니다.');
   }
 }
 
