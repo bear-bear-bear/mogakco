e45c9c2c703b30b9ddef397cb928fbf7dc575ac2
bearbearbear <bear-bear-bear@kakao.com>
2021-06-30 00:34:58 +0900

    fix: globalStyles 타입 에러 해결

--- next/components/globalStyles/index.tsx
+++ next/components/globalStyles/index.tsx
@@ -5,54 +5,53 @@ import FontFace from './_FontFace';
 import Variables from './_Variables';
 import media from './media';
 
+export const customGlobalStyles = css`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  html {
+    font-family: sans-serif;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -webkit-font-smoothing: antialiased;
+    text-rendering: optimizeLegibility;
+
+    ${media.xs} {
+      font-size: 14.5px;
+    }
+    ${media.sm} {
+      font-size: 15px;
+    }
+    ${media.md} {
+      font-size: 15px;
+    }
+    ${media.lg} {
+      font-size: 15.5px;
+    }
+    ${media.xl} {
+      font-size: 16px;
+    }
+    ${media.xxl} {
+      font-size: 16px;
+    }
+  }
+
+  body {
+    // TODO: 폰트 렌더링 최적화하기
+    font-family: 'Noto Sans KR', 'Roboto', 'HelveticaNeue', 'Helvetica Neue',
+      sans-serif;
+  }
+`;
+
 const GlobalStyles = (props: JSX.IntrinsicAttributes & GlobalProps) => (
   <>
     <Global styles={Resets} />
     <Global styles={Variables} />
     <Global styles={FontFace} />
-    <Global
-      {...props}
-      styles={css`
-        *,
-        *::before,
-        *::after {
-          box-sizing: border-box;
-        }
-
-        html {
-          font-family: sans-serif;
-          -ms-text-size-adjust: 100%;
-          -webkit-text-size-adjust: 100%;
-          -webkit-font-smoothing: antialiased;
-          text-rendering: optimizeLegibility;
-
-          ${media.xs} {
-            font-size: 14.5px;
-          }
-          ${media.sm} {
-            font-size: 15px;
-          }
-          ${media.md} {
-            font-size: 15px;
-          }
-          ${media.lg} {
-            font-size: 15.5px;
-          }
-          ${media.xl} {
-            font-size: 16px;
-          }
-          ${media.xxl} {
-            font-size: 16px;
-          }
-        }
-
-        body {
-          // TODO: 폰트 렌더링 최적화하기
-          font-family: 'Noto Sans KR', 'Roboto', 'HelveticaNeue',
-            'Helvetica Neue', sans-serif;
-        }
-      `}
-    />
+    <Global {...props} />
   </>
 );
 
--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,11 +1,11 @@
 import type { AppProps } from 'next/app';
 import wrapper from '~/redux/store/configureStore';
-import GlobalStyle from '~/components/globalStyles';
+import GlobalStyles, { customGlobalStyles } from '~/components/globalStyles';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <GlobalStyle />
+      <GlobalStyles styles={customGlobalStyles} />
       <Component {...pageProps} />
     </>
   );
