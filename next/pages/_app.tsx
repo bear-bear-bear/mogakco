280011b9cb0be254d1dffd79da37aae6f8c663ad
최은기 <galaxyhi4276@gmail.com>
2021-07-01 20:31:36 +0900

    feature: SWR Global Setting

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -2,13 +2,25 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import wrapper from '@redux/store/configureStore';
 import GlobalStyles, { customGlobalStyles } from '@components/globalStyles';
+import { SWRConfig, SWRConfiguration } from 'swr';
 
+/**
+ * @url SWR Options Description https://swr.vercel.app/docs/options
+ */
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider resetCSS={false}>
-      <GlobalStyles styles={customGlobalStyles} />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <SWRConfig
+      value={{
+        errorRetryCount: 3,
+        dedupingInterval: 2000,
+        errorRetryInterval: 5000,
+      }}
+    >
+      <ChakraProvider resetCSS={false}>
+        <GlobalStyles styles={customGlobalStyles} />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SWRConfig>
   );
 };
 
