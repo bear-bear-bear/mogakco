374e2d29c02a93f4c0e6bba423d3cad9faaf3e03
최은기 <galaxyhi4276@gmail.com>
2021-07-01 16:05:19 +0900

    feature: (swr) useSignUp Hooks 관리

--- next/pages/signup.tsx
+++ next/pages/signup.tsx
@@ -3,16 +3,16 @@ import { useRouter } from 'next/router';
 import { connect, useDispatch } from 'react-redux';
 import { END } from 'redux-saga';
 
-import wrapper from '@redux/store/configureStore';
-import { resetSignUp, verifyEmailRequest } from '@redux/reducers/signup';
-import useTypedSelector from '@hooks/useTypedSelector';
-import CustomHead from '@components/common/CustomHead';
-import AuthContainer from '@components/common/AuthContainer';
-import ProgressBar from '@components/signup/ProgressBar';
-import Start from '@components/signup/Start';
-import RequiredInfo from '@components/signup/RequiredInfo';
-import OptionalInfo from '@components/signup/OptionalInfo';
-import End from '@components/signup/End';
+import useSignUp from '@hooks/useSignUp';
+import wrapper from '~/redux/store/configureStore';
+import { verifyEmailRequest } from '~/redux/reducers/signup';
+import CustomHead from '~/components/common/CustomHead';
+import AuthContainer from '~/components/common/AuthContainer';
+import ProgressBar from '~/components/signup/ProgressBar';
+import Start from '~/components/signup/Start';
+import RequiredInfo from '~/components/signup/RequiredInfo';
+import OptionalInfo from '~/components/signup/OptionalInfo';
+import End from '~/components/signup/End';
 
 interface Props {
   isQuery: boolean;
@@ -26,19 +26,11 @@ const pageProps = {
 };
 
 const SignUp = ({ isQuery }: Props) => {
+  const { isSaveRequiredInfo, isSignUpDone, isVerifyEmail, updateSignUp } =
+    useSignUp();
   const router = useRouter();
-  const dispatch = useDispatch();
-  const verifyEmailDone = useTypedSelector(
-    ({ signup }) => signup.verifyEmailDone,
-  );
-  const verifySocialDone = useTypedSelector(
-    ({ signup }) => signup.verifySocialDone,
-  );
-  const saveRequiredInfoDone = useTypedSelector(
-    ({ signup }) => signup.saveRequiredInfoDone,
-  );
-  const signUpDone = useTypedSelector(({ signup }) => signup.signUpDone);
-  const fill = [verifyEmailDone, saveRequiredInfoDone, signUpDone];
+
+  const fill = [isVerifyEmail, isSaveRequiredInfo, isSignUpDone];
 
   useEffect(() => {
     // 페이지 떠날 때 모든 state 초기화
@@ -68,7 +60,8 @@ const SignUp = ({ isQuery }: Props) => {
 };
 
 // 이메일 검증 링크를 타고 이 페이지로 들어와 관련 쿼리가 있다면,
-// 해당 쿼리로 이후의 로직 실행
+// 해당 쿼리로 이후의 로직 실행\
+// TODO: 리덕스 로직 분리
 export const getServerSideProps = wrapper.getServerSideProps(
   async ({ query, store }) => {
     const { success, email: verifiedEmail } = query;
