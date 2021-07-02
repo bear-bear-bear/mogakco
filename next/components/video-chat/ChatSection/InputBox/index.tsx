8ddccf26ada2f1e99d470f759f8ae292ce1a89c4
최은기 <galaxyhi4276@gmail.com>
2021-07-02 16:44:53 +0900

    feature: 채팅 박스  챗 타이핑 작성

--- next/components/video-chat/ChatSection/InputBox/index.tsx
+++ next/components/video-chat/ChatSection/InputBox/index.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { KeyboardEvent, useRef } from 'react';
+import useInput from '@hooks/useInput';
+import log from 'loglevel';
+import { socketServer } from '@pages/chat/[id]';
 import * as S from './style';
 
+log.setLevel('DEBUG');
+
+const resize = (ref?: HTMLTextAreaElement): void => {
+  if (ref) {
+    ref.style.height = '1px';
+    ref.style.height = `${12 + ref.scrollHeight}px`;
+  }
+};
+
 const InputBox = () => {
+  const [chat, onChangeChat, setChat] = useInput('');
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+
+  const handleChat = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (chat.trim() !== '') {
+      if (e.code === 'Enter' && !e.altKey) {
+        resize(textAreaRef.current);
+        socketServer.emit('chat', chat);
+        setChat('');
+      }
+      if (e.code === 'Enter' && e.altKey) {
+        setChat((prev) => `${prev}\n`);
+      }
+    }
+  };
+
   return (
     <S.InputBox>
       <S.Header>
         <S.FileAddButton />
       </S.Header>
-      {/* 아래 Temp 붙은 친구들은 editer 라이브러리 도입 전 임시 form을 담당합니다. */}
-      <S.TempEditorForm action="" method="POST">
-        <S.TempTextArea name="" placeholder="여기에 메세지 입력 ..." />
-        <S.TempSendButton />
-      </S.TempEditorForm>
+      <S.TempTextArea
+        value={chat}
+        onKeyDown={handleChat}
+        onChange={onChangeChat}
+        placeholder="여기에 메세지 입력..."
+      />
+      <S.TempSendButton />
     </S.InputBox>
   );
 };
