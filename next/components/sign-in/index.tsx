acaa41b88ffd0dc326e8b0e31e865183a4a124ab
bearbearbear <bear-bear-bear@kakao.com>
2021-07-03 00:19:29 +0900

    feat: 로그인 기능에서 누락된 부분 수정, login > sign-in or signIn 이름 변경

--- next/components/sign-in/index.tsx
+++ next/components/sign-in/index.tsx
@@ -10,12 +10,10 @@ import Label from '@components/common/Label';
 import useDebugLog from '@hooks/useDebugLog';
 import { logInFetcher } from '@lib/fetchers';
 import { logAxiosError } from '@lib/apiClient';
-import { emailRule, passwordRule } from '@lib/regex';
 
 // TODO: 회원가입 컴포넌트 스타일 그대로 갖다쓰는데, 해당 스타일 공용화 시키기
 import * as CS from '@components/sign-up/common/styles';
 import * as S from '@components/sign-up/Start/style';
-import isAllPropertyTruthy from '@lib/isAllPropertyTruthy';
 
 export type FormInputs = {
   email: string | null;
@@ -24,7 +22,7 @@ export type FormInputs = {
 
 const SignInForm = () => {
   const router = useRouter();
-  const [loginLoading, setLoginLoading] = useState<boolean>(false);
+  const [signInLoading, setLoginLoading] = useState<boolean>(false);
   const [isVisiblePassword, setIsVisiblePassword] = useState(false);
   const onClickEye = () => setIsVisiblePassword((prev) => !prev);
   const debugLog = useDebugLog();
@@ -36,12 +34,13 @@ const SignInForm = () => {
     debugLog('미구현 기능입니다');
   };
 
-  const onSubmit = (loginInfo: UnpackNestedValue<FormInputs>) => {
-    console.log('loginInfo', loginInfo);
+  const onSubmit = (signInInfo: UnpackNestedValue<FormInputs>) => {
     setLoginLoading(true);
-    logInFetcher(loginInfo as ILoginProps)
+    logInFetcher(signInInfo as ILoginProps)
       .then(() => {
         // TODO: 서비스 페이지로 이동하기
+        debugLog('로그인 성공 응답');
+        debugLog('서비스 페이지 미구현 상태이므로 임시 경로(/)로 이동합니다');
         setLoginLoading(false);
         router.push('/');
       })
@@ -95,7 +94,7 @@ const SignInForm = () => {
             {...register('password', { required: true })}
           />
         </InputWrapper>
-        <CS.SubmitButton type="submit" fullWidth $loading={loginLoading}>
+        <CS.SubmitButton type="submit" fullWidth $loading={signInLoading}>
           이메일로 로그인
         </CS.SubmitButton>
       </Form>
