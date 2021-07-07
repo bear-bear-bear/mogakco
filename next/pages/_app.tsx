4f95242cd9e67f952c83beac3f85f8779f9159a4
최은기 <galaxyhi4276@gmail.com>
2021-07-07 13:28:05 +0900

    feature: 여러 탭 사용할 때 한 탭에서 로그아웃 시 다른 탭에서 감지

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -2,11 +2,28 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import GlobalStyles, { customGlobalStyles } from '@components/globalStyles';
 import { SWRConfig } from 'swr';
+import { useEffect } from 'react';
+import { refreshAccessTokenApi } from '@lib/fetchers';
+import { Memory, memoryStore } from '@lib/apiClient';
 
 /**
  * @url SWR Options Description https://swr.vercel.app/docs/options
  */
 const App = ({ Component, pageProps }: AppProps) => {
+  /**
+   * @desc 다른 페이지에서 로그아웃 할 시 감지하여 AccessToken 을 지워버립니다.
+   */
+  useEffect(() => {
+    const refreshStore = async () => {
+      refreshAccessTokenApi().catch(() => {
+        memoryStore.delete(Memory.ACCESS_TOKEN);
+      });
+    };
+
+    window.addEventListener('storage', refreshStore);
+    return () => window.removeEventListener('storage', refreshStore);
+  });
+
   return (
     <SWRConfig
       value={{
