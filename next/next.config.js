7b661eb63abc610e491c6b68dbcc0dda5623c099
bearbearbear <bear-bear-bear@kakao.com>
2021-07-10 19:38:36 +0900

    feat: 변경될 pages 구조에 맞춰 미리 redirection 설정

--- next/next.config.js
+++ next/next.config.js
@@ -14,4 +14,45 @@ module.exports = {
 
     return config;
   },
+  async redirects() {
+    return [
+      // 인가 서비스, 비인가 서비스 구분 리다이렉션
+      {
+        source: '/',
+        has: [
+          {
+            type: 'header',
+            key: 'x-authorized',
+            value: 'true',
+          },
+        ],
+        permanent: false,
+        destination: '/service/',
+      },
+      {
+        source: '/auth/:path*',
+        has: [
+          {
+            type: 'header',
+            key: 'x-authorized',
+            value: 'true',
+          },
+        ],
+        permanent: false,
+        destination: '/service/',
+      },
+      {
+        source: '/service/:path*',
+        has: [
+          {
+            type: 'header',
+            key: 'x-authorized',
+            value: 'false',
+          },
+        ],
+        permanent: false,
+        destination: '/',
+      },
+    ];
+  },
 };
