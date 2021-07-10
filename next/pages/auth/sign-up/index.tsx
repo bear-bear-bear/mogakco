fa21dc3be1f90908b120df5d1f50c442583556e4
bearbearbear <bear-bear-bear@kakao.com>
2021-07-10 19:47:26 +0900

    style: 기존 ssr로 인가 확인하던 로직 임시 주석 처리

--- next/pages/auth/sign-up/index.tsx
+++ next/pages/auth/sign-up/index.tsx
@@ -27,29 +27,29 @@ const SignUpStart = () => {
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async ({
-  req: { headers },
-}) => {
-  log.setLevel('DEBUG');
-  try {
-    const {
-      data: { accessToken },
-    } = await refreshAccessTokenApiSSR(headers);
-    memoryStore.set(Memory.ACCESS_TOKEN, accessToken);
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    };
-  } catch (err) {
-    if (err.statusCode === 401) {
-      if (isDevelopment) {
-        log.debug('비로그인 상태입니다.');
-      }
-    }
-    return { props: {} };
-  }
-};
+// export const getServerSideProps: GetServerSideProps = async ({
+//   req: { headers },
+// }) => {
+//   log.setLevel('DEBUG');
+//   try {
+//     const {
+//       data: { accessToken },
+//     } = await refreshAccessTokenApiSSR(headers);
+//     memoryStore.set(Memory.ACCESS_TOKEN, accessToken);
+//     return {
+//       redirect: {
+//         destination: '/',
+//         permanent: false,
+//       },
+//     };
+//   } catch (err) {
+//     if (err.statusCode === 401) {
+//       if (isDevelopment) {
+//         log.debug('비로그인 상태입니다.');
+//       }
+//     }
+//     return { props: {} };
+//   }
+// };
 
 export default SignUpStart;
