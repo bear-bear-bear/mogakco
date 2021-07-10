32b0fe752ff634ca3264b7f0e56e49d04737e21e
bearbearbear <bear-bear-bear@kakao.com>
2021-07-10 23:20:06 +0900

    feat: 첫 렌더링 시 쿠키에 라우팅을 위한 값을 세팅하고 RELOAD하도록 설정

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,8 +1,10 @@
-import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
-import GlobalStyles, { customGlobalStyles } from '@components/globalStyles';
-import { SWRConfig } from 'swr';
 import { useEffect } from 'react';
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { SWRConfig } from 'swr';
+import { ChakraProvider } from '@chakra-ui/react';
+
+import GlobalStyles, { customGlobalStyles } from '@components/globalStyles';
 import { Memory, memoryStore } from '@lib/apiClient';
 
 // export const END_POINT = 'http://localhost:8001/chat';
@@ -11,6 +13,7 @@ import { Memory, memoryStore } from '@lib/apiClient';
  * @url SWR Options Description https://swr.vercel.app/docs/options
  */
 const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
   /**
    * @desc 다른 페이지에서 로그아웃 할 시 감지하여 AccessToken 을 지워버립니다.
    */
@@ -26,6 +29,18 @@ const App = ({ Component, pageProps }: AppProps) => {
     window.addEventListener('storage', detectLogOut);
     return () => window.removeEventListener('storage', detectLogOut);
   }, []);
+
+  /**
+   * @desc 첫 렌더링 시 next/config/js 리다이렉션 라우팅을 위한 쿠키 세팅
+   */
+  useEffect(() => {
+    if (document.cookie.includes('authorized') === false) {
+      document.cookie = 'authorized=false';
+      // 첫 렌더링 시엔 next.config.js에 설정한 리다이렉션 라우팅이 적용이 안되지만,
+      // 리로드하면 next.config.js의 리다이렉션 조건 검사가 가능해집니다.
+      router.reload();
+    }
+  }, [router]);
 
   return (
     <SWRConfig
