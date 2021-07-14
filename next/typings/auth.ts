1f5daa4e86ae891131998b18a69dd701b150e1bd
bearbearbear <bear-bear-bear@kakao.com>
2021-07-15 01:17:22 +0900

    style: 타입 네임 컨벤션 수정

--- next/typings/auth.ts
+++ next/typings/auth.ts
@@ -4,26 +4,27 @@ type Response = {
 };
 
 // 로그인 요청에 필요한 정보
-export interface ILoginProps {
+export interface ISignInProps {
   email: string;
   password: string;
 }
 
 // 회원가입 요청에 필요한 정보
-export interface ISignUpUserProps {
-  email: string | null;
-  username: string | null;
-  password: string | null;
+export interface ISignUpProps {
+  email: string;
+  username: string;
+  password: string;
   skills: number[] | null;
   job: number | null;
 }
 
-// 회원가입 optional 페이지에서 서버로부터 받는 선택 옵션들의 타입
-export interface IOptionalProps {
-  id: number;
-  name: string;
-  createdAt: Date;
-  updatedAt: Date;
+// 회원가입/로그인 성공 응답 스키마
+export interface IAuthSuccessResponse {
+  statusCode: number;
+  message: string;
+  accessToken: string;
+  expiration: string;
+  user: IUserProps;
 }
 
 // 유저가 가지고 있는 정보
@@ -35,24 +36,17 @@ export interface IUserProps {
   job: IOptionalProps | null;
 }
 
-// 회원가입 성공 응답 스키마
-export interface ISignUpResponse {
-  statusCode: number;
-  message: string;
-  accessToken: string;
-  expiration: string;
-}
-
-// 로그인 성공 응답 스키마
-export interface ILoginSuccessResponse extends ISignUpResponse {
-  user: IUserProps;
-  accessToken: string;
-  expiration: string;
-}
-
 // accessToken 연장 성공 응답 스키마
 export interface IProlongTokenProps extends Response {
   user: IUserProps;
   accessToken: string;
   expiration: string;
+}
+
+// 회원가입 optional 페이지에서 서버로부터 받는 선택 옵션들의 타입
+export interface IOptionalProps {
+  id: number;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
