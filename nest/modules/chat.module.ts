d43cc07e59cba7b47946f725e98b2a483d3e0512
galaxy4276 <galaxyhi4276@gmail.com>
2021-07-16 17:11:14 +0900

    FIX: Nest 버그 및 소켓 부분 작성 (#247)
    * remove: 사용하지 않는 Repository 삭제
    
    * fix: 공용 타입이 Nest에서 문제를 일으키는 부분 수정
    
    * feature: 임시 useSocket 작성
    
    * feature: 기존 소켓 방식 제거

--- nest/modules/chat.module.ts
+++ nest/modules/chat.module.ts
@@ -4,9 +4,12 @@ import RoomRepository from '@models/repositories/room.repository';
 import ChatController from '@controllers/chat.controller';
 import ChatGateway from '@controllers/chat.gateway';
 import ChatService from '@services/chat.service';
+import AuthModule from '@modules/auth.module';
+import UserModule from '@modules/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RoomRepository])],
+  imports: [TypeOrmModule.forFeature([RoomRepository]), AuthModule, UserModule],
+  // imports: [TypeOrmModule.forFeature([RoomRepository])],
   controllers: [ChatController],
   providers: [ChatGateway, ChatService],
 })
