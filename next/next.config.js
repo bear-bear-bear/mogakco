857bee80f5081d9874c79f2e2f192d90b20c6c3d
bearbearbear <bear-bear-bear@kakao.com>
2021-07-17 03:42:07 +0900

    feat: test 관련 파일은 next 빌드에서 제거

--- next/next.config.js
+++ next/next.config.js
@@ -2,14 +2,18 @@
  * @type {import('next/dist/next-server/server/config').NextConfig}
  * */
 module.exports = {
+  excludeFile: (str) => /test\/*/.test(str),
   webpack: (config) => {
     config.module.rules.push({
-      // 웹팩설정에 로더 추가함
       test: /\.svg$/,
       issuer: {
         test: /\.(js|ts)x?$/,
       },
       use: ['@svgr/webpack'],
+    });
+    config.module.rules.push({
+      test: /\.(test|spec).(js|ts|tsx)$/,
+      loader: 'ignore-loader',
     });
 
     return config;
