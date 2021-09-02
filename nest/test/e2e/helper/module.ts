92b1654df720a621dd2b09a2892244717b076194
최은기 <galaxyhi4276@gmail.com>
2021-09-03 08:33:26 +0900

    feat: 테스트 Application 작성 및 반환 함수 작성

---
+++ nest/test/e2e/helper/module.ts
@@ -0,0 +1,35 @@
+import { Test } from '@nestjs/testing';
+import AppModule from '@modules/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+
+/**
+ * @isValid Validation 사용
+ * @isCookieAble CookieParser 사용
+ */
+interface Options {
+  isValid?: boolean;
+  isCookieAble: boolean;
+}
+
+export default async function getTestAppModule(opts: Options): Promise<INestApplication> {
+  const moduleRef = await Test.createTestingModule({
+    imports: [AppModule],
+  }).compile();
+  const app: INestApplication = moduleRef.createNestApplication();
+  app.setGlobalPrefix('api');
+  if (opts.isValid) {
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+  }
+  if (opts.isCookieAble) {
+    app.use(cookieParser());
+  }
+  await app.init();
+  return app;
+}
