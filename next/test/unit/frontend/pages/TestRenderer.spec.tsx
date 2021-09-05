8a4c28efeafb6dfcbed54615baa5b13ea8464efa
Kim June Jae <bear-bear-bear@kakao.com>
2021-09-05 23:50:13 +0900

    feat: test 관련 모듈과 디렉터리 삭제 (#266)
    * feat: test 관련 모듈과 디렉터리 삭제
    
    * feat: 프런트 테스트와 패키지 설치 관련 CI 명령 삭제
    
    * feat: lint npx 제거하고 수동설치로 변경
    
    * fix: 롤백

--- next/test/unit/frontend/pages/TestRenderer.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import TestRenderer from '../components/TestRenderer.test';
-
-describe('리액트 테스트 케이스', () => {
-  test('렌더링 테스트', () => {
-    render(<TestRenderer />);
-    screen.getByText('여행기분떡상하기');
-  });
-});
