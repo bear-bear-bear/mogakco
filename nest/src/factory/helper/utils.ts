3ae6041b0962b46612351f4db652a5336b36028b
최은기 <galaxyhi4276@gmail.com>
2021-09-06 00:43:27 +0900

    remove: 더 이상 사용하지 않는 디렉터리 구조 삭제

--- nest/src/factory/helper/utils.ts
+++
@@ -1,3 +0,0 @@
-export const getRandomCount = (count: number) => Math.floor(Math.random() * count);
-
-export default {};
--- nest/src/models/repositories/user-field.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import UserFieldEntity from '../entities/user-field.entity';
-
-@EntityRepository(UserFieldEntity)
-export default class UserFieldRepository extends Repository<UserFieldEntity> {}
