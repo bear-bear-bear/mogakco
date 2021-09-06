9db041f7804dc2ab428ba6d3b24b4bf7a23c86f6
최은기 <galaxyhi4276@gmail.com>
2021-09-06 17:13:22 +0900

    feat: Dto 에 ApiProperty 작성

--- nest/src/authentication/dto/create-user.dto.ts
+++ nest/src/authentication/dto/create-user.dto.ts
@@ -9,6 +9,7 @@ import {
   MinLength,
 } from 'class-validator';
 import UserJobEntity from '@models/user/entities/users-job.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 const passwordRegex = new RegExp(
   /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@!%*#?&])[A-Za-z\d$@!%*#?&]{8,50}$/,
@@ -17,6 +18,7 @@ const nickNameRegex = new RegExp(/^[^ㄱ-ㅎ]?[가-힣a-zA-Z0-9.]{1,12}$/);
 const emptyMessage = '빈 값이 올 수 없습니다.';
 
 export default class CreateUserDto {
+  @ApiProperty({ description: '닉네임' })
   @IsNotEmpty({ message: emptyMessage })
   @IsString()
   @MinLength(1, { message: '닉네임은 최소 1글자 이상이어야 합니다.' })
@@ -24,22 +26,26 @@ export default class CreateUserDto {
   @Matches(nickNameRegex, { message: '닉네임 형식이 맞지 않습니다.' })
   readonly username!: string;
 
+  @ApiProperty({ description: '이메일' })
   @IsNotEmpty({ message: emptyMessage })
   @IsString()
   @IsEmail()
   @MaxLength(50, { message: '이메일은 50글자를 넘을 수 없습니다.' })
   readonly email!: string;
 
+  @ApiProperty({ description: '패스워드' })
   @IsNotEmpty({ message: emptyMessage })
   @IsString()
   @MaxLength(15, { message: '패스워드는 15자 이상을 넘을 수 없습니다.' })
   @Matches(passwordRegex, { message: '패스워드 형식이 맞지 않습니다.' })
   readonly password!: string;
 
+  @ApiProperty({ description: '관심 분야 ( 최대 5개 )' })
   @IsOptional()
   @ArrayMaxSize(5, { message: '관심 분야는 5개 이상 등록 할 수 없습니다.' })
   skills!: number[] | null;
 
+  @ApiProperty({ description: '직업' })
   @IsOptional()
   job!: UserJobEntity | null;
 }
--- nest/src/authentication/dto/login-user.dto.ts
+++ nest/src/authentication/dto/login-user.dto.ts
@@ -1,9 +1,12 @@
 import { IsEmail, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export default class LoginUserDto {
+  @ApiProperty({ description: '이메일' })
   @IsEmail()
   readonly email!: string;
 
+  @ApiProperty({ description: '비밀번호' })
   @IsString()
   readonly password!: string;
 }
