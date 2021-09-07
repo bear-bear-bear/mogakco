e0516033517579d3dd6bf6588db1bf0fb486c8ee
최은기 <galaxyhi4276@gmail.com>
2021-09-07 10:59:44 +0900

    feat: 사용자 레포지토리 ORM 코드 기능 추가 및 리팩터링

--- nest/src/models/user/repositories/user.repository.ts
+++ nest/src/models/user/repositories/user.repository.ts
@@ -1,12 +1,16 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, getManager, Repository } from 'typeorm';
 import { InternalServerErrorException } from '@nestjs/common';
 import CreateUserDto from '@authentication/dto/create-user.dto';
 import UserFieldEntity from '../entities/user-field.entity';
 import UserEntity from '../entities/user.entity';
-import UserJobEntity from '../entities/users-job.entity';
+
+type UserEntityType = UserEntity | undefined;
 
 @EntityRepository(UserEntity)
 class UserRepository extends Repository<UserEntity> {
+  /**
+   * @desc 유저 생성
+   */
   async createUserOne({
     username,
     password,
@@ -25,31 +29,28 @@ class UserRepository extends Repository<UserEntity> {
     return newUser;
   }
 
-  async findUserOne(id: number) {
-    const user = await this.findOne({ id });
-    if (!user) {
-      throw new InternalServerErrorException();
-    }
-    return user;
+  /**
+   * @desc id 에 해당하는 유저를 찾는다.
+   */
+  findUserOne(id: number): Promise<UserEntityType> {
+    return this.findOne({ id });
   }
 
-  async findUserByName(username: string) {
-    const user = await this.findOne({
+  /**
+   * @desc username 에 해당하는 유저를 찾는다.
+   */
+  findUserByName(username: string): Promise<UserEntityType> {
+    return this.findOne({
       where: { username },
       select: ['username'],
     });
-    if (!user) {
-      return null;
-    }
-    return user;
   }
 
-  async findUserByEmail(email: string) {
-    const user = await this.findOne({ email });
-    if (!user) {
-      return null;
-    }
-    return user;
+  /**
+   * @desc email 에 해당하는 유저를 찾는다.
+   */
+  findUserByEmail(email: string): Promise<UserEntityType> {
+    return this.findOne({ email });
   }
 
   /**
@@ -57,83 +58,40 @@ class UserRepository extends Repository<UserEntity> {
    * @desc 클라이언트, 사용자에게 제공되어야 할 객체만을 전달합니다.
    */
   async findUserByEmailForLogin(email: string) {
-    const user = await this.findOne(
-      { email },
-      {
-        select: ['id', 'username', 'email', 'password', 'skills', 'job'],
-      },
-    );
-
-    if (user === undefined) return null;
-
-    // TODO: Relation Column 개선 필요
-    const skillIds = user.skills;
-    const skills =
-      skillIds &&
-      (await UserFieldEntity.findByIds(skillIds, {
-        select: ['id', 'name'],
-      }));
-    const job = await UserJobEntity.findOne({
-      select: ['id', 'name'],
-      where: { id: user.id },
-    });
+    const user = await this.getJoinJobQueryBuilder()
+      .where('user.email = :email', { email })
+      .getOne();
+    if (!user) return null;
+    const skills = await this.getUserFields(user.skills);
     return {
       ...user,
       skills,
-      job,
     };
   }
 
   async findUserByIdForLogin(id: number) {
-    const user = await this.findOne(id, {
-      select: ['id', 'username', 'email', 'password', 'skills', 'job'],
-    });
-
+    const user = await this.getJoinJobQueryBuilder().where('user.email = :email', { id }).getOne();
     if (user === undefined) return null;
-
-    // TODO: Relation Column 개선 필요
-    const skillIds = user.skills;
-    const skills =
-      skillIds &&
-      (await UserFieldEntity.findByIds(skillIds, {
-        select: ['id', 'name'],
-      }));
-    const job = await UserJobEntity.findOne({
-      select: ['id', 'name'],
-      where: { id: user.id },
-    });
+    const skills = await this.getUserFields(user.skills);
     return {
       ...user,
       skills,
-      job,
     };
   }
 
   /**
-   * @description 민감한 데이터 제거, 중복 함수가 많기때문에 차선책을 고려할 예정 TODO: 차선책 요구
+   * @description 민감한 데이터 제거
    */
   async findUserShallow(id: number) {
-    const user = await this.findOne(id, {
-      select: ['id', 'username', 'email', 'skills', 'job'],
-    });
-
+    const user = await this.getJoinJobQueryBuilder()
+      .select(['id', 'username', 'email', 'skills', 'job'])
+      .where('user.email = :email', { id })
+      .getOne();
     if (user === undefined) return null;
-
-    // TODO: Relation Column 개선 필요
-    const skillIds = user.skills;
-    const skills =
-      skillIds &&
-      (await UserFieldEntity.findByIds(skillIds, {
-        select: ['id', 'name'],
-      }));
-    const job = await UserJobEntity.findOne({
-      select: ['id', 'name'],
-      where: { id: user.id },
-    });
+    const skills = await this.getUserFields(user.skills);
     return {
       ...user,
       skills,
-      job,
     };
   }
 
@@ -147,6 +105,36 @@ class UserRepository extends Repository<UserEntity> {
 
   async deleteUser(id: number) {
     return this.softDelete(id);
+  }
+
+  /**
+   * @desc skill id 리스트를 받아서 객체를 반환합니다.
+   * @return [{ id, name }, ...]
+   */
+  getUserFields(list: number[] | null): Promise<UserFieldEntity[]> | null {
+    if (!list) return null;
+    return getManager().findByIds(UserFieldEntity, list, {
+      select: ['id', 'name'],
+    });
+  }
+
+  /**
+   * @desc 사용자 id를 받아서 해당하는 직업 정보 객체를 반환합니다.
+   * @return { id, name }
+   */
+  getUserJob(id: number) {
+    return getManager().findOne(UserFieldEntity, {
+      where: {
+        id,
+      },
+    });
+  }
+
+  /**
+   * @desc users_job 과 join 이 연결된 쿼리 빌더를 반환합니다.
+   */
+  getJoinJobQueryBuilder() {
+    return this.createQueryBuilder('user').leftJoinAndSelect('user.job', 'user_job');
   }
 }
 
