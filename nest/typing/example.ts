a8c5f271d0bc85f2f79e35f97047ae90c1733623
최은기 <galaxyhi4276@gmail.com>
2021-09-07 23:09:48 +0900

    remove: 사용하지 않는 타입 제거

--- nest/typing/example.ts
+++
@@ -1,7 +0,0 @@
-/** @interface Types of Data Object & Properties  */
-
-/** @typing Types of Data Object & Properties with Utilities Class */
-
-/** @Middlewares Types of Middlewares ( guards, interceptors, pipes, middlewares ) */
-
-/** @Dto Data Transfer Object ( Class ) */
