8b66ce4163d0c1385422f44d72f87fce7c25fabe
최은기 <galaxyhi4276@gmail.com>
2021-09-10 06:56:04 +0900

    feature: chat.module 변경사항 반영

--- nest/src/models/chat/chat.module.ts
+++ nest/src/models/chat/chat.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import UserModule from '@models/user/user.module';
-import AuthModule from '@authentication//auth.module';
+import AuthModule from '@authentication/modules/auth.module';
 import RoomRepository from './repositories/room.repository';
 import ChatController from './chat.controller';
 import ChatGateway from './chat.gateway';
 import ChatService from './chat.service';
+import UserRepository from '@models/user/repositories/user.repository';
+import RoomUserRepository from '@models/chat/repositories/room-user.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RoomRepository]), AuthModule, UserModule],
+  imports: [
+    TypeOrmModule.forFeature([RoomRepository, UserRepository, RoomUserRepository]),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [ChatController],
   providers: [ChatGateway, ChatService],
 })
-class ChatModule {}
-
-export default ChatModule;
+export default class ChatModule {}
