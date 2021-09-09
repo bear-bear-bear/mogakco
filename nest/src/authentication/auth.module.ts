70f57c59ab7f490a12cb5e4ce8951329de3f88f5
최은기 <galaxyhi4276@gmail.com>
2021-09-09 18:34:50 +0900

    feat: 모듈 관심사 분리

--- nest/src/authentication/auth.module.ts
+++ nest/src/authentication/auth.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import EmailService from '@mail/email.service';
@@ -8,41 +6,20 @@ import UserRepository from '@models/user/repositories/user.repository';
 import UserVerifyRepository from '@models/user/repositories/user-verify.repository';
 import UserJobRepository from '@models/user/repositories/ user-job.reposity';
 import UserModule from '@models/user/user.module';
-import JwtStrategyWithRefresh from './strategies/jwt.refresh.strategy';
-import JwtStrategy from './strategies/jwt.strategy';
 
 import AuthController from './auth.controller';
-import AuthService from './auth.service';
-import AuthValidateService from '@authentication/auth-validate.service';
+import JwtModule from '@authentication/jwt.module';
+import NonAuthGuard from '@common/guards/non-auth.guard';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserVerifyRepository, UserRepository, UserJobRepository]),
-    PassportModule.register({
-      defaultStrategy: ['jwt', 'jwt-with-refresh'],
-    }),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_ACCESS_TOKEN_SECRET'),
-        signOptions: {
-          expiresIn: `${configService.get<string | number>('JWT_ACCESS_TOKEN_EXPIRATION_TIME')}s`,
-        },
-      }),
-    }),
+    JwtModule,
     UserModule,
     ConfigModule,
   ],
   controllers: [AuthController],
-  providers: [
-    ConfigService,
-    AuthService,
-    AuthValidateService,
-    JwtStrategy,
-    JwtStrategyWithRefresh,
-    EmailService,
-  ],
-  exports: [AuthService],
+  providers: [ConfigService, EmailService, NonAuthGuard],
+  exports: [],
 })
 export default class AuthModule {}
