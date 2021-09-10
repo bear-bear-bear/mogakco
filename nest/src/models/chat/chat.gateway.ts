55b1badff484adcb10bdd7f22e020340567c49ef
최은기 <galaxyhi4276@gmail.com>
2021-09-10 09:49:12 +0900

    feat: 소켓 서버 작성

--- nest/src/models/chat/chat.gateway.ts
+++ nest/src/models/chat/chat.gateway.ts
@@ -6,39 +6,71 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-import { Logger } from '@nestjs/common';
+import { InternalServerErrorException, Logger, UnauthorizedException } from '@nestjs/common';
 import ChatService from './chat.service';
+import UserRepository from '@models/user/repositories/user.repository';
 
-/**
- * @ClientIdentifier client 의 conn.id
- */
-@WebSocketGateway({ namespace: 'chat', cors: { origin: '*', credentials: true } })
+interface IJoinChatRoom {
+  userId: number;
+  roomId: string;
+}
+
+@WebSocketGateway({
+  namespace: 'chat',
+  cors: { origin: '*', credentials: true },
+})
 class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server!: Server;
 
   private logger = new Logger('Chat');
 
-  constructor(private readonly chatService: ChatService) {}
+  constructor(
+    private readonly chatService: ChatService,
+    private readonly userRepository: UserRepository,
+  ) {}
 
   handleConnection(client: Socket) {
+    const username = client.handshake.headers['user-name'];
+    if (username === 'no-user') throw new InternalServerErrorException();
+    client.broadcast.emit('enterRoom', `${username} 이 입장하였습니다.`);
     this.logger.debug(`client ${client.conn.id} connection!`);
   }
 
-  handleDisconnect(client: Socket) {
-    client.disconnect();
+  // TODO: 단순화 가능성 보임
+  async handleDisconnect(client: Socket) {
+    const userId = client.handshake.headers['user-id'];
+    if (!userId) throw new InternalServerErrorException();
+    const user = await this.userRepository.findOne({ id: Number(userId) });
+    if (!user) throw new InternalServerErrorException();
+    const room = await this.chatService.findBelongToRoomByUserId(user);
+    await this.chatService.exitRoom(user, room);
+    client.broadcast.emit('exitUser', `${user.username} 유저가 퇴장하였습니다.`);
+    this.logger.log(`${user.username} 유저가 ${room.id} 방에서 퇴장하였습니다.`);
     this.logger.debug(`client ${client.conn.id} disconnected`);
   }
 
   @SubscribeMessage('joinChatRoom')
-  async joinChatRoom(client: Socket, roomId: string) {
+  async joinChatRoom(client: Socket, { roomId, userId }: IJoinChatRoom) {
+    const user = await this.userRepository.findOne({ id: userId });
+    if (!user) throw new UnauthorizedException();
+    await this.chatService.joinRoom(user, Number(roomId));
     this.server.to(roomId).emit('joinUserMessage', client.conn.id);
-    return client.join(roomId);
+    return {
+      joinedUserName: user.username,
+      joinedRoomId: roomId,
+    };
   }
 
+  // TODO: 개발 중
   @SubscribeMessage('chat')
-  async chat(client: Socket, roomId: string) {
-    console.log(client, roomId);
+  async chat(client: Socket, message: string) {
+    const username = client.handshake.headers['user-name'] as string;
+    client.broadcast.emit('chat', {
+      username,
+      message,
+      type: 'chat',
+    });
   }
 }
 
