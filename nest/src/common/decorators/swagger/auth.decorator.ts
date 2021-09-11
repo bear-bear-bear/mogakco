e62051ee932cf3ef8c1d5a7200e51d815d1e0821
galaxy4276 <galaxyhi4276@gmail.com>
2021-09-11 11:51:22 +0900

    test: swagger response case 보충 및 HttpStatus 교체

--- nest/src/common/decorators/swagger/auth.decorator.ts
+++ nest/src/common/decorators/swagger/auth.decorator.ts
@@ -1,19 +1,57 @@
 import { ApiBearerAuth, ApiOperation, ApiQuery, ApiResponse } from '@nestjs/swagger';
 import { SwaggerTag } from '@common/helpers/enum.helper';
 import decoratorHelper from '@common/helpers/decorator.helper';
+import { HttpStatus } from '@nestjs/common';
+
+const mockUser = {
+  id: 1,
+  username: 'username',
+  email: 'email',
+  job: {
+    id: 1,
+    name: '직업',
+    createdAt: 'Date',
+    updatedAt: 'Date',
+  },
+  skills: [
+    {
+      id: 1,
+      name: '주술사',
+      createdAt: 'Date',
+      updatedAt: 'Date',
+    },
+    {
+      id: 2,
+      name: '성기사',
+      createdAt: 'Date',
+      updatedAt: 'Date',
+    },
+  ],
+};
 
 export function LoginSwagger() {
   return decoratorHelper(
     SwaggerTag.AUTH,
     ApiOperation({ summary: '로그인 API', description: '로그인을 수행합니다.' }),
-    ApiResponse({ status: 200, description: '로그인 성공' }),
-    ApiResponse({ status: 400, description: '잘못된 요청 폼' }),
+    ApiResponse({
+      status: HttpStatus.OK,
+      description: '로그인 성공',
+      schema: {
+        example: {
+          statusCode: HttpStatus.OK,
+          message: '로그인에 성공하였습니다!',
+          expiration: new Date(),
+          accessToken: '$a2@asdvcxv@231S',
+          user: mockUser,
+        },
+      },
+    }),
+    ApiResponse({ status: HttpStatus.BAD_REQUEST, description: '잘못된 요청 폼' }),
     ApiResponse({ status: 404, description: '해당 정보의 유저가 존재하지 않음' }),
-    ApiResponse({ status: 401, description: '비밀번호가 틀림' }),
+    ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: '비밀번호가 틀림' }),
   );
 }
 
-// TODO: Response Case 보충
 export function LogoutSwagger() {
   return decoratorHelper(
     SwaggerTag.AUTH,
@@ -21,8 +59,17 @@ export function LogoutSwagger() {
       summary: '로그아웃 API',
       description: '로그아웃을 수행합니다.',
     }),
-    ApiResponse({ status: 200, description: '로그아웃 성공' }),
-    ApiResponse({ status: 401, description: 'refreshToken 정보 없음' }),
+    ApiResponse({
+      status: HttpStatus.OK,
+      description: '로그아웃 성공',
+      schema: {
+        example: {
+          message: `loginUser 유저가 로그아웃 되었습니다.`,
+          statusCode: HttpStatus.OK,
+        },
+      },
+    }),
+    ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: 'refreshToken 정보 없음' }),
   );
 }
 
@@ -33,8 +80,20 @@ export function AccessTokenSwagger() {
       summary: 'accessToken 갱신 API',
       description: 'accessToken 을 갱신하여 반환합니다. ( Refresh 정보 필요 )',
     }),
-    ApiResponse({ status: 201, description: 'accessToken 갱신 완료' }),
-    ApiResponse({ status: 401, description: 'refreshToken 정보가 없음' }),
+    ApiResponse({
+      status: HttpStatus.CREATED,
+      description: 'accessToken 갱신 완료',
+      schema: {
+        example: {
+          accessToken: 'acac@ascac@',
+          expiration: new Date(),
+          message: 'accessToken 갱신 완료!',
+          statusCode: HttpStatus.CREATED,
+          user: mockUser,
+        },
+      },
+    }),
+    ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: 'refreshToken 정보가 없음' }),
   );
 }
 
@@ -44,48 +103,24 @@ export function SignSwagger() {
     ApiBearerAuth(),
     ApiOperation({ summary: '회원가입 API', description: '회원가입을 수행합니다.' }),
     ApiResponse({
-      status: 201,
+      status: HttpStatus.CREATED,
       description: '회원가입 성공',
       schema: {
         example: {
-          statusCode: 200,
+          statusCode: HttpStatus.OK,
           message: '회원가입에 성공하였습니다.',
           accessToken: 'acecssToken',
           expiration: 'Date',
-          user: {
-            id: 1,
-            username: 'username',
-            email: 'email',
-            job: {
-              id: 1,
-              name: '직업',
-              createdAt: 'Date',
-              updatedAt: 'Date',
-            },
-            skills: [
-              {
-                id: 1,
-                name: '주술사',
-                createdAt: 'Date',
-                updatedAt: 'Date',
-              },
-              {
-                id: 2,
-                name: '성기사',
-                createdAt: 'Date',
-                updatedAt: 'Date',
-              },
-            ],
-          },
+          user: mockUser,
         },
       },
     }),
-    ApiResponse({ status: 400, description: '잘못된 요청 폼' }),
+    ApiResponse({ status: HttpStatus.BAD_REQUEST, description: '잘못된 요청 폼' }),
     ApiResponse({
-      status: 409,
+      status: HttpStatus.CONFLICT,
       description: '이미 존재하는 유저 정보를 사용',
     }),
-    ApiResponse({ status: 401, description: '이메일 인증을 수행하지 않음' }),
+    ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: '이메일 인증을 수행하지 않음' }),
   );
 }
 
@@ -97,8 +132,17 @@ export function SendTokenSwagger() {
       description:
         '입력 이메일 정보로 인증정보를 전송하고 이메일 전송 여부에 대한 객체를 반환합니다.',
     }),
-    ApiResponse({ status: 200, description: '이메일 전송 성공' }),
-    ApiResponse({ status: 400, description: '잘못된 요청 폼' }),
+    ApiResponse({
+      status: HttpStatus.OK,
+      description: '이메일 전송 성공',
+      schema: {
+        example: {
+          statusCode: HttpStatus.OK,
+          message: '이메일 전송 성공',
+        },
+      },
+    }),
+    ApiResponse({ status: HttpStatus.BAD_REQUEST, description: '잘못된 요청 폼' }),
   );
 }
 
@@ -117,7 +161,10 @@ export function BeforeRegisterSwagger() {
       name: 'email',
       description: '인증 유저의 이메일',
     }),
-    ApiResponse({ status: 307, description: '회원가입 페이지로 리다이렉션 됨' }),
+    ApiResponse({
+      status: HttpStatus.TEMPORARY_REDIRECT,
+      description: '회원가입 페이지로 리다이렉션 됨',
+    }),
   );
 }
 
@@ -133,8 +180,18 @@ export function VerifyEmailSwagger() {
       name: 'email',
       description: '검증받을 유저의 이메일 정보',
     }),
-    ApiResponse({ status: 200, description: '이메일 인증을 수행한 유저' }),
-    ApiResponse({ status: 401, description: '인증을 수행하지 않은 유저' }),
+    ApiResponse({
+      status: HttpStatus.OK,
+      description: '이메일 인증을 수행한 유저',
+      schema: {
+        example: {
+          statusCode: HttpStatus.OK,
+          message: true,
+          email: 'email@email.com',
+        },
+      },
+    }),
+    ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: '인증을 수행하지 않은 유저' }),
   );
 }
 
@@ -147,6 +204,15 @@ export function GetAuthenticationSwagger() {
         'Bearer Access Token 기반으로 로그인 유효한 사용자인 지 여부와 유저 객체를 반환합니다.',
     }),
     ApiBearerAuth(),
-    ApiResponse({ status: 200, description: '인증 여부와 유저 객체 반환' }),
+    ApiResponse({
+      status: HttpStatus.OK,
+      description: '인증 여부와 유저 객체 반환 ( 로그인 상태 )',
+      schema: {
+        example: {
+          isLoggedIn: true,
+          ...mockUser,
+        },
+      },
+    }),
   );
 }
--- nest/src/common/decorators/swagger/chat.decorator.ts
+++ nest/src/common/decorators/swagger/chat.decorator.ts
@@ -1,6 +1,7 @@
 import decoratorHelper from '@common/helpers/decorator.helper';
 import { SwaggerTag } from '@common/helpers/enum.helper';
 import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
+import { HttpStatus } from '@nestjs/common';
 
 export function ChatAvailableSwagger() {
   return decoratorHelper(
@@ -10,11 +11,17 @@ export function ChatAvailableSwagger() {
       description: '해당 id 의 채팅방이 이용가능 한지 여부를 반환합니다.',
     }),
     ApiResponse({
-      status: 200,
+      status: HttpStatus.OK,
       description: '채팅방 이용 가능',
+      schema: {
+        example: {
+          statusCode: HttpStatus.OK,
+          message: '채팅방이 존재합니다.',
+        },
+      },
     }),
     ApiResponse({
-      status: 400,
+      status: HttpStatus.BAD_REQUEST,
       description: '채팅방 이용 불가',
     }),
   );
@@ -32,11 +39,16 @@ export function GetRoomMembersSwagger() {
       description: '채팅방 번호를 Parameter 로 받아서 해당 방에 존재하는 유저 수를 찾습니다.',
     }),
     ApiResponse({
-      status: 200,
+      status: HttpStatus.OK,
       description: '채팅방 인원 수 반환',
+      schema: {
+        example: {
+          memberCount: 5,
+        },
+      },
     }),
     ApiResponse({
-      status: 400,
+      status: HttpStatus.BAD_REQUEST,
       description: '존재하지 않는 채팅방',
     }),
   );
