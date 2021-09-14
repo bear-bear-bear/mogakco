0015df665194255bfcd2a735e3faf6dfc1bdf784
bearbearbear <bear-bear-bear@kakao.com>
2021-09-14 20:44:57 +0900

    feat: access-control-expose-headers 추가

--- next/next.config.js
+++ next/next.config.js
@@ -11,6 +11,27 @@ const nextConfig = {
 
     return config;
   },
+
+  async headers() {
+    return [
+      // if the header `x-add-header` is present,
+      // the `x-another-header` header will be applied
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'Set-Cookie',
+          },
+        ],
+        headers: [
+          {
+            key: 'access-control-expose-headers',
+            value: 'Set-Cookie',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
