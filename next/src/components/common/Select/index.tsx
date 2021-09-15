599dc3179d19a46ada722d8c3efcd860748fad3b
bearbearbear <bear-bear-bear@kakao.com>
2021-09-15 17:42:06 +0900

    refactor: <Select> 공용 컴포넌트 리팩터링

--- next/src/components/common/Select/index.tsx
+++ next/src/components/common/Select/index.tsx
@@ -1,73 +1,23 @@
-import React, { ComponentProps, VFC } from 'react';
-import ReactSelect, { OptionTypeBase, StylesConfig } from 'react-select';
+import React, { ComponentProps } from 'react';
+import ReactSelect from 'react-select';
 import makeAnimated from 'react-select/animated';
 
-const animatedComponents = makeAnimated();
+import stylesConfig from './stylesConfig';
 
-interface IForwardProps {
+interface SelectProps {
   props: ComponentProps<typeof ReactSelect>;
 }
 
-type Styles = {
-  colourStyles: StylesConfig<OptionTypeBase, true>;
-};
+const MultipleSelect = ({ props }: SelectProps) => (
+  <ReactSelect
+    isMulti
+    components={makeAnimated()}
+    placeholder="항목을 선택해주세요"
+    classNamePrefix="react-select-container"
+    styles={stylesConfig}
+    noOptionsMessage={() => '항목이 존재하지 않습니다.'}
+    {...props}
+  />
+);
 
-// TODO: GlobalVariables 적용
-const Select: VFC<ComponentProps<typeof ReactSelect>> = (props) => {
-  const colourStyles: StylesConfig<any, any> = {
-    container: (styles) => ({
-      ...styles,
-      width: '100%',
-    }),
-    control: (styles) => ({
-      ...styles,
-      borderRadius: 0,
-    }),
-    dropdownIndicator: (base) => ({
-      ...base,
-      borderRadius: 0,
-    }),
-    menu: (base) => ({
-      ...base,
-      borderRadius: 0,
-      maxWidth: 500,
-    }),
-    input: (base) => ({
-      ...base,
-      borderRadius: 0,
-    }),
-    multiValue: (base) => ({
-      ...base,
-      backgroundColor: '#ffffff',
-      border: '1px solid #c9d1d9',
-    }),
-    multiValueRemove: (base) => ({
-      ...base,
-      ':hover': {
-        backgroundColor: '#f23f31',
-        color: '#f0f6fc',
-      },
-    }),
-  };
-
-  return <ForwardMultipleSelect props={props} colourStyles={colourStyles} />;
-};
-
-const ForwardMultipleSelect: VFC<IForwardProps & Styles> = ({
-  colourStyles,
-  props,
-}) => {
-  return (
-    <ReactSelect
-      isMulti
-      components={animatedComponents}
-      placeholder="항목을 선택해주세요"
-      classNamePrefix="react-select-container"
-      styles={colourStyles}
-      noOptionsMessage={() => '항목이 존재하지 않습니다.'}
-      {...props}
-    />
-  );
-};
-
-export default Select;
+export default MultipleSelect;
--- next/src/lib/toSelectOptions.ts
+++ next/src/lib/toSelectOptions.ts
@@ -3,16 +3,18 @@ import type { IOptionalProps } from 'typings/auth';
 /**
  * @desc Select 라이브러리 props 형식에 맞게 skills와 jobs 데이터를 가공해주는 함수
  */
-export type SelectProps = {
-  value: number;
+// TODO typings/auth.ts 로 옮기기
+export interface SelectProps {
   label: string;
-};
+  value: string;
+}
+
 const toSelectOptions = (list: IOptionalProps[] | null) => {
   if (list === null) return [];
 
   return list.map(
     ({ id, name }): SelectProps => ({
-      value: id,
+      value: id.toString(),
       label: name,
     }),
   );
