959a1ca11222ed61a85650dccc122b3fc7514155
최은기 <galaxyhi4276@gmail.com>
2021-09-15 09:53:05 +0900

    feat: socket 훅스 개선

--- next/src/hooks/useSocket.tsx
+++ next/src/hooks/useSocket.tsx
@@ -1,41 +1,31 @@
-import useSWR from 'swr';
-import { io } from 'socket.io-client';
-import { useEffect } from 'react';
-import devModeLog from '@lib/devModeLog';
+import { io, Socket } from 'socket.io-client';
+import { useEffect, useState } from 'react';
 import useUser from '@hooks/useUser';
 import { useRouter } from 'next/router';
 
-export const SOCKET_TEST_KEY = 'socket-test';
-const SOCKET_SERVER = 'http://localhost:8001/chat';
-
 export default function useSocket() {
-  const { query } = useRouter();
-  const roomId = query.id as string;
+  const [socket, setSocket] = useState<Socket | null>(null);
   const { user } = useUser();
-  const { data: client, mutate } = useSWR(
-    SOCKET_TEST_KEY,
-    () =>
-      io(SOCKET_SERVER, {
-        reconnectionDelay: 2000,
-        extraHeaders: {
-          'user-id': String(user?.id),
-          'user-name': user?.username || 'no-user',
-          'room-id': roomId,
-        },
-      }),
-    {
-      revalidateOnFocus: false,
-    },
-  );
+  const { query } = useRouter();
   useEffect(() => {
-    return () => {
-      devModeLog('disconnect');
-      client?.disconnect();
-    };
-  }, [client]);
+    console.log('소켓 클라이언트 생성');
+    const client = io('http://localhost:8001/chat', {
+      auth: {
+        'user-id': user?.id,
+        'room-id': query.id,
+        'user-name': user?.username,
+      },
+    });
+    setSocket(client);
 
-  return {
-    client,
-    mutate,
-  };
+    function cleanup() {
+      client.disconnect();
+    }
+
+    return cleanup;
+    // 한번 만 렌더링 되어야 합니다.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return socket;
 }
