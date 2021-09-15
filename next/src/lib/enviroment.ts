bbe4c55bbe5bcf0c6aebb6893dbc19c957945f64
bearbearbear <bear-bear-bear@kakao.com>
2021-09-16 08:21:58 +0900

    refactor: getServerUrl 리팩터링

--- next/src/lib/enviroment.ts
+++ next/src/lib/enviroment.ts
@@ -1,32 +1,17 @@
-export const isDevelopment = process.env.NODE_ENV === 'development';
-export const isTest = process.env.NODE_ENV === 'test';
-export const isProduction = process.env.NODE_ENV === 'production';
+const { NODE_ENV, TEST_SERVER_URL, PROD_SERVER_URL } = process.env;
 
 /**
  * @desc 환경과 모드에 맞는 서버 url 을 반환합니다.
  * @param mode 채팅 서버인 지, http 서버인 지 판별
  */
 export const getServerUrl = (mode?: 'http' | 'socket'): string => {
-  if (mode === 'socket') {
-    if (isTest) {
-      const url = process.env.TEST_SERVER_URL as string;
-      return `${url}/chat`;
-    }
-    if (isProduction) {
-      const url = process.env.PROD_SERVER_URL as string;
-      return `${url}/chat`;
-    }
-    return 'http://localhost:8001/chat';
+  const route = mode === 'socket' ? '/chat' : '';
+  switch (NODE_ENV) {
+    case 'test':
+      return `${TEST_SERVER_URL}${route}`;
+    case 'production':
+      return `${PROD_SERVER_URL}${route}`;
+    default:
+      return `http://localhost:8001${route}`;
   }
-
-  if (mode === 'http') {
-    if (isTest) {
-      return process.env.TEST_SERVER_URL as string;
-    }
-    if (isProduction) {
-      return process.env.PROD_SERVER_URL as string;
-    }
-    return 'http://localhost:8001';
-  }
-  return 'http://localhost:8001';
 };
