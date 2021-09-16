d692830ff62ff7a2119ce866754056bab06aa8f2
bearbearbear <bear-bear-bear@kakao.com>
2021-09-16 18:04:57 +0900

    style: 미사용 변수와 테스트 로그 제거

--- next/src/lib/enviroment.ts
+++ next/src/lib/enviroment.ts
@@ -7,11 +7,6 @@ export const isProduction = process.env.NODE_ENV === 'production';
  * @param mode 채팅 서버인 지, http 서버인 지 판별
  */
 export const getServerUrl = (mode?: 'http' | 'socket'): string => {
-  console.log({
-    NODE_ENV: process.env.NODE_ENV,
-    TEST_SERVER_URL: process.env.NEXT_PUBLIC_TEST_SERVER_URL,
-    PROD_SERVER_URL: process.env.PROD_SERVER_URL,
-  });
   const route = mode === 'socket' ? '/chat' : '';
   switch (process.env.NODE_ENV) {
     case 'test':
