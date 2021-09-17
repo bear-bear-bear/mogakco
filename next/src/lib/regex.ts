556478c3e5fceab1317471bcfb1897219b29c436
bearbearbear <bear-bear-bear@kakao.com>
2021-09-18 05:58:57 +0900

    fix: 이메일 규식형 수정

--- next/src/lib/regex.ts
+++ next/src/lib/regex.ts
@@ -3,7 +3,7 @@ export const usernameRule = /^[^ㄱ-ㅎ]?[가-힣a-zA-Z0-9.]{1,12}$/;
 
 // email: 이메일
 export const emailRule =
-  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
 
 // password: 비밀번호, 영문자, 숫자, 특수문자 각각 1개 이상 포함한 8-50자
 export const passwordRule =
