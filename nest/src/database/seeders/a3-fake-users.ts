e62c71b12c841caf5f4e6a1b1f5d524f950d1f32
galaxy4276 <galaxyhi4276@gmail.com>
2021-09-18 17:54:15 +0900

    refactor: 리팩터링 및 develoment 전용 시더 설정

--- nest/src/database/seeders/a3-fake-users.ts
+++ nest/src/database/seeders/a3-fake-users.ts
@@ -5,38 +5,41 @@ import { getRandomFieldList, getRandomJob } from '@common/helpers/test.helper';
 import { Connection } from 'typeorm';
 import UserRolesRepository from '@src/models/user/repositories/user-roles.repository';
 import RolesRepository from '@models/user/repositories/roles.repository';
+import { ServerEnviroment } from '@common/helpers/enum.helper';
 
 export default class FakeUserSeed implements Seeder {
   async run(factory: Factory, connection: Connection) {
-    const users = await factory(UserEntity)()
-      .map(async ({ username, email, password: plain }: UserEntity): Promise<UserEntity> => {
-        const password = await makeHash(plain);
-        const returnUser = new UserEntity();
-        returnUser.username = username;
-        returnUser.password = password;
-        returnUser.email = email;
-        returnUser.skills = await getRandomFieldList();
-        returnUser.job = await getRandomJob();
-        return returnUser;
-      })
-      .createMany(30);
+    if (process.env.NODE_ENV === ServerEnviroment.DEV) {
+      const users = await factory(UserEntity)()
+        .map(async ({ username, email, password: plain }: UserEntity): Promise<UserEntity> => {
+          const password = await makeHash(plain);
+          const returnUser = new UserEntity();
+          returnUser.username = username;
+          returnUser.password = password;
+          returnUser.email = email;
+          returnUser.skills = await getRandomFieldList();
+          returnUser.job = await getRandomJob();
+          return returnUser;
+        })
+        .createMany(30);
 
-    const role = await connection.getCustomRepository(RolesRepository).findOne({
-      where: { name: '일반 사용자' },
-    });
+      const role = await connection.getCustomRepository(RolesRepository).findOne({
+        where: { name: '일반 사용자' },
+      });
 
-    const createRolePromises = users.map(
-      user =>
-        new Promise((resolve, reject) => {
-          const userRole = connection.getCustomRepository(UserRolesRepository).create({
-            user,
-            role,
-          });
-          if (!userRole) reject(new Error('역할이 없음'));
-          resolve(userRole.save());
-        }),
-    );
+      const createRolePromises = users.map(
+        user =>
+          new Promise((resolve, reject) => {
+            const userRole = connection.getCustomRepository(UserRolesRepository).create({
+              user,
+              role,
+            });
+            if (!userRole) reject(new Error('역할이 없음'));
+            resolve(userRole.save());
+          }),
+      );
 
-    await Promise.all(createRolePromises);
+      await Promise.all(createRolePromises);
+    }
   }
 }
--- nest/src/database/seeders/a3-test-users.ts
+++ nest/src/database/seeders/a3-test-users.ts
@@ -5,68 +5,78 @@ import UserEntity from '@models/user/entities/user.entity';
 import makeHashHelper from '@common/helpers/make-hash.helper';
 import RolesEntity from '@models/user/entities/roles.entity';
 import UserRolesEntity from '@src/models/user/entities/user-roles.entity';
+import { ServerEnviroment } from '@common/helpers/enum.helper';
+
+/**
+ * @desc mogauser, mogauser2 에게 일반 사용자 권한을 부여합니다.
+ */
+async function setTestUserRoles(connection: Connection) {
+  const role = await connection
+    .createQueryBuilder()
+    .select()
+    .from(RolesEntity, 'roles')
+    .where('roles.name = :name', { name: '일반 사용자' })
+    .getOne();
+
+  await connection
+    .createQueryBuilder()
+    .insert()
+    .into(UserRolesEntity)
+    .values({
+      user: await connection
+        .createQueryBuilder()
+        .select()
+        .from(UserEntity, 'user')
+        .where('user.username = :username', { username: 'mogauser' })
+        .getOne(),
+      role,
+    });
+
+  await connection
+    .createQueryBuilder()
+    .insert()
+    .into(UserRolesEntity)
+    .values({
+      user: await connection
+        .createQueryBuilder()
+        .select()
+        .from(UserEntity, 'user')
+        .where('user.username = :username', { username: 'mogauser2' })
+        .getOne(),
+      role,
+    });
+}
 
 export default class TestUserSeed implements Seeder {
   async run(factory: Factory, connection: Connection): Promise<void> {
-    const email = process.env.EMAIL_ADMIN as string;
-    const skills = await getRandomFieldList();
-    const job = await getRandomJob();
-    const password = await makeHashHelper('mogapass');
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(UserEntity)
-      .values([
-        {
-          username: 'mogauser',
-          password,
-          email,
-          skills,
-          job,
-        },
-        {
-          username: 'mogauser2',
-          password,
-          email: `${email}a`,
-          skills,
-          job,
-        },
-      ])
-      .execute();
+    if (process.env.NODE_ENV === ServerEnviroment.DEV) {
+      const email = process.env.EMAIL_ADMIN as string;
+      const skills = await getRandomFieldList();
+      const job = await getRandomJob();
+      const password = await makeHashHelper('mogapass');
+      await connection
+        .createQueryBuilder()
+        .insert()
+        .into(UserEntity)
+        .values([
+          {
+            username: 'mogauser',
+            password,
+            email,
+            skills,
+            job,
+          },
+          {
+            username: 'mogauser2',
+            password,
+            email: `${email}a`,
+            skills,
+            job,
+          },
+        ])
+        .execute();
 
-    const role = await connection
-      .createQueryBuilder()
-      .select()
-      .from(RolesEntity, 'roles')
-      .where('roles.name = :name', { name: '일반 사용자' })
-      .getOne();
-
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(UserRolesEntity)
-      .values({
-        user: await connection
-          .createQueryBuilder()
-          .select()
-          .from(UserEntity, 'user')
-          .where('user.username = :username', { username: 'mogauser' })
-          .getOne(),
-        role,
-      });
-
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(UserRolesEntity)
-      .values({
-        user: await connection
-          .createQueryBuilder()
-          .select()
-          .from(UserEntity, 'user')
-          .where('user.username = :username', { username: 'mogauser2' })
-          .getOne(),
-        role,
-      });
+      await setTestUserRoles(connection);
+    }
   }
 }
