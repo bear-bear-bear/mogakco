2c5401e2db7f91fdb39ac9b5109311ac57df3b64
galaxy4276 <galaxyhi4276@gmail.com>
2021-09-18 17:36:32 +0900

    feat: 가짜 유저들 기존 테스트 개선 및 역할 부여 작성

---
+++ nest/src/database/seeders/a3-fake-users.ts
@@ -0,0 +1,42 @@
+import { Factory, Seeder } from 'typeorm-seeding';
+import UserEntity from '@models/user/entities/user.entity';
+import makeHash from '@common/helpers/make-hash.helper';
+import { getRandomFieldList, getRandomJob } from '@common/helpers/test.helper';
+import { Connection } from 'typeorm';
+import UserRolesRepository from '@src/models/user/repositories/user-roles.repository';
+import RolesRepository from '@models/user/repositories/roles.repository';
+
+export default class FakeUserSeed implements Seeder {
+  async run(factory: Factory, connection: Connection) {
+    const users = await factory(UserEntity)()
+      .map(async ({ username, email, password: plain }: UserEntity): Promise<UserEntity> => {
+        const password = await makeHash(plain);
+        const returnUser = new UserEntity();
+        returnUser.username = username;
+        returnUser.password = password;
+        returnUser.email = email;
+        returnUser.skills = await getRandomFieldList();
+        returnUser.job = await getRandomJob();
+        return returnUser;
+      })
+      .createMany(30);
+
+    const role = await connection.getCustomRepository(RolesRepository).findOne({
+      where: { name: '일반 사용자' },
+    });
+
+    const createRolePromises = users.map(
+      user =>
+        new Promise((resolve, reject) => {
+          const userRole = connection.getCustomRepository(UserRolesRepository).create({
+            user,
+            role,
+          });
+          if (!userRole) reject(new Error('역할이 없음'));
+          resolve(userRole.save());
+        }),
+    );
+
+    await Promise.all(createRolePromises);
+  }
+}
