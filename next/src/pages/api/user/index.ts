1c951b21010c2feb290f888ca4ae951451d02389
Kim June Jae <bear-bear-bear@kakao.com>
2021-09-19 17:14:55 +0900

    fix: 유저 put 요청 타입에 id 추가, api 가독성 개선 (#349)

--- next/src/pages/api/user/index.ts
+++ next/src/pages/api/user/index.ts
@@ -1,8 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import type { IAccountEditProps, IUserInfo } from 'typings/auth';
 
+const fakeDB = {
+  update: (userInfoToUpdate: IAccountEditProps): IUserInfo => {
+    const updatedUser = {
+      ...userInfoToUpdate,
+      skills: [
+        {
+          id: 13,
+          name: '풀스택',
+        },
+      ],
+      job: {
+        id: 8,
+        name: '보안 관제',
+      },
+    };
+    return updatedUser;
+  },
+};
+
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  const userInfo: IAccountEditProps = req.body;
+  const infoToUpdate: IAccountEditProps = req.body;
 
   if (req.method !== 'PUT') {
     res.status(405).json({
@@ -12,32 +31,14 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
     return;
   }
 
-  console.log('다음 정보로 PUT 요청이 왔지롱', userInfo);
+  console.log('다음 정보로 PUT 요청이 왔지롱', infoToUpdate);
 
-  const changedUser: IUserInfo = {
-    id: 1,
-    email: 'mogakco35@gmail.com',
-    username: 'mogauser',
-    skills: [
-      {
-        id: 1,
-        name: '리눅스',
-      },
-      {
-        id: 13,
-        name: '풀스택',
-      },
-    ],
-    job: {
-      id: 8,
-      name: '보안 관제',
-    },
-  };
+  const updatedUser: IUserInfo = fakeDB.update(infoToUpdate);
 
   res.status(200).json({
     message: '계정 정보가 성공적으로 수정되었습니다.',
     statusCode: 200,
-    user: changedUser,
+    user: updatedUser,
   });
 };
 
