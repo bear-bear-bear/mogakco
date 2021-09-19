17abf841f02df1c50600be2c629b2fdaf31a90bb
최은기 <galaxyhi4276@gmail.com>
2021-09-19 18:35:51 +0900

    feat: 수정, 삭제 Swagger 작성

--- nest/src/common/decorators/swagger/user.decorator.ts
+++ nest/src/common/decorators/swagger/user.decorator.ts
@@ -1,6 +1,6 @@
 import decoratorHelper from '@common/helpers/decorator.helper';
 import { SwaggerTag } from '@common/helpers/enum.helper';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { HttpStatus } from '@nestjs/common';
 
 export function GetAllFieldSwagger() {
@@ -57,6 +57,91 @@ export function GetAllJobSwagger() {
               name: '총통',
             },
           ],
+        },
+      },
+    }),
+  );
+}
+
+export function UpdateUserSwagger() {
+  return decoratorHelper(
+    SwaggerTag.USER,
+    ApiOperation({
+      summary: '사용자 수정 API',
+      description: '사용자 인증 요청에서 수정 데이터를 받아 유저 정보를 수정합니다.',
+    }),
+    ApiBearerAuth(),
+    ApiResponse({
+      status: HttpStatus.OK,
+      description: '사용자 수정 성공',
+      schema: {
+        example: {
+          statusCode: 200,
+          message: '계정 정보가 성공적으로 수정되었습니다.',
+          user: {
+            id: 32,
+            username: 'mogat',
+            email: 'mogakco35@gmail.com',
+            skills: [
+              {
+                id: 1,
+                name: '안드로이드',
+              },
+              {
+                id: 2,
+                name: '백엔드',
+              },
+              {
+                id: 3,
+                name: '컴파일러',
+              },
+            ],
+            job: {
+              id: 1,
+              name: '대학원생',
+            },
+          },
+        },
+      },
+    }),
+    ApiResponse({
+      status: HttpStatus.UNAUTHORIZED,
+      description: '유저 권한이 없음',
+      schema: {
+        example: {
+          status: HttpStatus.UNAUTHORIZED,
+          message: 'Unauthorized',
+        },
+      },
+    }),
+  );
+}
+
+export default function DeleteUserSwagger() {
+  return decoratorHelper(
+    SwaggerTag.USER,
+    ApiOperation({
+      summary: '유저 삭제 API',
+      description: '유저 인증 정보를 받아 해당 유저 데이터를 삭제합니다.',
+    }),
+    ApiBearerAuth(),
+    ApiResponse({
+      status: HttpStatus.OK,
+      description: '유저 삭제 성공',
+      schema: {
+        example: {
+          statusCode: HttpStatus.OK,
+          message: '계정이 성공적으로 삭제되었습니다.',
+        },
+      },
+    }),
+    ApiResponse({
+      status: HttpStatus.UNAUTHORIZED,
+      description: '유저 권한이 없음',
+      schema: {
+        example: {
+          status: HttpStatus.UNAUTHORIZED,
+          message: 'Unauthorized',
         },
       },
     }),
