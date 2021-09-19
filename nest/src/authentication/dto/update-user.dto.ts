4fd5bc044c34310f3df61eaa4053a694650446c1
최은기 <galaxyhi4276@gmail.com>
2021-09-19 22:30:17 +0900

    fix: dto 검증 누락 프로퍼티 수정

--- nest/src/authentication/dto/update-user.dto.ts
+++ nest/src/authentication/dto/update-user.dto.ts
@@ -1,4 +1,10 @@
-import { OmitType } from '@nestjs/swagger';
+import { ApiProperty, OmitType } from '@nestjs/swagger';
 import CreateUserDto from '@authentication/dto/create-user.dto';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
-export default class UpdateUserDto extends OmitType(CreateUserDto, ['password'] as const) {}
+export default class UpdateUserDto extends OmitType(CreateUserDto, ['password'] as const) {
+  @ApiProperty({ description: '아이디' })
+  @IsNotEmpty({ message: '빈 값이 올 수 없습니다.' })
+  @IsNumber()
+  id!: number;
+}
