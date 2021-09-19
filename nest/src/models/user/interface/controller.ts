7e95bfbea32225e54d312aea4a0967633faa285e
최은기 <galaxyhi4276@gmail.com>
2021-09-19 18:37:05 +0900

    feat: 유저 수정 / 삭제 타입 작성

--- nest/src/models/user/interface/controller.ts
+++ nest/src/models/user/interface/controller.ts
@@ -1,12 +1,20 @@
-import { Request } from 'express';
+import { Request, Response } from 'express';
+import UpdateUserDto from '@authentication/dto/update-user.dto';
+import { ShallowUser } from '@models/user/interface/service';
+import { GeneralResponse } from '@common/interface/global';
+
+export interface UpdateUser {
+  statusCode: number;
+  message: string;
+  user?: ShallowUser;
+  error?: string;
+}
 
 export type AuthRequest = Request & { user: { id: number; username: string } };
 
 export type UserSelectableProp = { id: number; name: string };
 
-export interface GetSelectableProps {
-  statusCode: number;
-  message: string;
+export interface GetSelectableProps extends GeneralResponse {
   list: UserSelectableProp[] | null;
 }
 
@@ -16,4 +24,8 @@ export interface IUserController {
   getAllFields(): Promise<GetSelectableProps>;
 
   getAllJobs(): Promise<GetSelectableProps>;
+
+  changeUserInfoFromMyPage(req: AuthRequest, updateUserDto: UpdateUserDto): Promise<UpdateUser>;
+
+  deleteUserFromMyPage(req: AuthRequest, res: Response): Promise<GeneralResponse>;
 }
