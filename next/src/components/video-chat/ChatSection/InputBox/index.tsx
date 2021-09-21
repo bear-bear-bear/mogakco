6aad3bdabcdd37c03e9fa2f1c2145f3a35605e6b
bearbearbear <ghlwkdthfl@gmail.com>
2021-09-22 04:23:36 +0900

    feat: send 버튼에도 전송기능을 부여하고, 키 이벤트 관련 기존 코드 리팩터링

--- next/src/components/video-chat/ChatSection/InputBox/index.tsx
+++ next/src/components/video-chat/ChatSection/InputBox/index.tsx
@@ -9,17 +9,26 @@ const InputBox = () => {
   const [chat, onChangeChat, setChat] = useInput('');
   const client = useContext(SocketContext);
 
-  const handleChat = (e: KeyboardEvent<HTMLTextAreaElement>) => {
-    if (chat.trim() !== '') {
-      const isEnter = e.code === 'Enter';
-      if (isEnter && !e.altKey) {
-        client?.emit('chat', chat);
-        setChat('');
-      }
-      if (isEnter && e.altKey) {
-        setChat((prev) => `${prev}\n`);
-      }
+  const sendChat = () => {
+    if (!client) return;
+
+    const clearedChat = chat.trim();
+    if (clearedChat === '') return;
+
+    client.emit('chat', clearedChat);
+    setChat('');
+  };
+
+  const handleEnterKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.code !== 'Enter') return;
+    e.preventDefault();
+
+    if (e.altKey) {
+      setChat((prev) => `${prev}\n`);
+      return;
     }
+
+    sendChat();
   };
 
   return (
@@ -29,11 +38,11 @@ const InputBox = () => {
       </S.Header>
       <S.TempTextArea
         value={chat}
-        onKeyDown={handleChat}
+        onKeyDown={handleEnterKeyDown}
         onChange={onChangeChat}
         placeholder="여기에 메세지 입력..."
       />
-      <S.TempSendButton />
+      <S.TempSendButton onClick={sendChat} />
     </S.InputBox>
   );
 };
