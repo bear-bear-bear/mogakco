9de78feb05ceba5c188100bf8ec67d6ed5daf4b7
bearbearbear <ghlwkdthfl@gmail.com>
2021-09-23 02:37:49 +0900

    feat: @uiw/react-md-editor 설치 및 관련 설정

--- next/next.config.js
+++ next/next.config.js
@@ -1,7 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const removeImports = require('next-remove-imports')({});
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  experimental: { esmExternals: true },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
@@ -13,4 +17,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = removeImports(nextConfig);
