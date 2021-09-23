853637105952151cc9594193889828d8d656f888
bearbearbear <ghlwkdthfl@gmail.com>
2021-09-23 19:51:21 +0900

    feat: 입력창에 에디터 추가

--- next/src/components/video-chat/ChatSection/InputBox/index.tsx
+++ next/src/components/video-chat/ChatSection/InputBox/index.tsx
@@ -1,26 +1,56 @@
-import React, { KeyboardEvent, useContext } from 'react';
+import React, { KeyboardEvent, useContext, useState } from 'react';
+import dynamic from 'next/dynamic';
+import { MDEditorCustomProps } from '@uiw/react-md-editor';
 
-import useInput from '@hooks/useInput';
+// https://github.com/uiwjs/react-md-editor/issues/52#issuecomment-848969341
+import '@uiw/react-md-editor/markdown-editor.css';
+import '@uiw/react-markdown-preview/markdown.css';
+
+// https://github.com/uiwjs/react-md-editor/issues/224#issuecomment-925673338
+import {
+  bold,
+  italic,
+  divider,
+  link,
+  quote,
+  code,
+  image,
+  unorderedListCommand,
+  group,
+} from '@uiw/react-md-editor/lib/commands';
+import type {
+  TextState,
+  TextAreaTextApi,
+} from '@uiw/react-md-editor/lib/commands';
+
+import UploadSVG from '@public/svg/upload.svg';
 import { SocketContext } from '@pages/video-chat/[id]';
 
 import * as S from './style';
 
+const MDEditor = dynamic<MDEditorCustomProps>(
+  () => import('@uiw/react-md-editor'),
+  {
+    ssr: false,
+  },
+);
+
 const InputBox = () => {
-  const [chat, onChangeChat, setChat] = useInput('');
+  const [chat, setChat] = useState<string | undefined>('');
   const client = useContext(SocketContext);
 
   const sendChat = () => {
     if (!client) return;
 
-    const clearedChat = chat.trim();
-    if (clearedChat === '') return;
+    const clearedChat = chat?.trim();
+    if (!clearedChat) return;
 
     client.emit('chat', clearedChat);
     setChat('');
   };
 
   const handleEnterKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.code !== 'Enter') return;
+    if (e.key !== 'Enter') return; // Not e.code (except composing)
     e.preventDefault();
 
     if (e.altKey) {
@@ -33,16 +63,35 @@ const InputBox = () => {
 
   return (
     <S.InputBox>
-      <S.Header>
-        <S.FileAddButton />
-      </S.Header>
-      <S.TempTextArea
+      <MDEditor
         value={chat}
+        onChange={setChat}
         onKeyDown={handleEnterKeyDown}
-        onChange={onChangeChat}
-        placeholder="여기에 메세지 입력..."
+        preview="edit"
+        commands={[
+          bold,
+          italic,
+          divider,
+          link,
+          quote,
+          code,
+          image,
+          divider,
+          unorderedListCommand,
+          divider,
+          group([], {
+            name: 'upload',
+            icon: UploadSVG(),
+            execute: (state: TextState, api: TextAreaTextApi) => {
+              alert('업로드 미구현');
+            },
+            buttonProps: {
+              title: 'Upload files',
+              'aria-label': 'Upload files',
+            },
+          }),
+        ]}
       />
-      <S.TempSendButton onClick={sendChat} />
     </S.InputBox>
   );
 };
