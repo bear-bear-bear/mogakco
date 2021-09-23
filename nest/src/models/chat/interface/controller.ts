30502a9efd95fdb665daac8cae4b72db6779537e
최은기 <galaxyhi4276@gmail.com>
2021-09-23 21:46:53 +0900

    feat: 서비스와 컨트롤러 인터페이스 구현부 작성

--- nest/src/models/chat/interface/controller.ts
+++ nest/src/models/chat/interface/controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { AuthRequest } from '@models/user/interface/controller';
 import { GeneralResponse } from '@common/interface/global';
 import AnonymousPropDto from '@models/chat/dto/anonymous-prop.dto';
+import AnonymousPrefixEntity from '@models/chat/entities/anonymous_prefix.entity';
 
 export interface AvailableRoom {
   message: string;
@@ -18,6 +19,19 @@ export interface ChatRoomJoin {
   message: string;
 }
 
+export interface UpdateAnonymousProp extends GeneralResponse {
+  id: number;
+  name: string;
+}
+
+export interface DeleteAnonymousProp extends GeneralResponse {
+  id: number;
+}
+
+export interface FindAllAnonymousProp extends GeneralResponse {
+  list: AnonymousPrefixEntity[] | null;
+}
+
 export interface IChatController {
   isAvailableChatRoom(roomId: number): Promise<AvailableRoom>;
 
@@ -26,16 +40,28 @@ export interface IChatController {
   join(req: Request, res: Response): Promise<ChatRoomJoin | Pick<ChatRoomJoin, 'message'>>;
 
   chatFileUpload(file: Express.Multer.File): Promise<any>;
-
   chatFileDownload(req: Request): any;
 
   addAnonymousPrefixRuleByAdmin(
     req: AuthRequest,
     prefixBody: AnonymousPropDto,
   ): Promise<GeneralResponse>;
+  modifyAnonymousPrefixRuleByAdmin(
+    id: number,
+    body: AnonymousPropDto,
+  ): Promise<UpdateAnonymousProp>;
+  deleteAnonymousPrefixRuleByAdmin(id: number): Promise<DeleteAnonymousProp>;
 
   addAnonymousNameRuleByAdmin(
     req: AuthRequest,
     prefixBody: AnonymousPropDto,
   ): Promise<GeneralResponse>;
+  modifyAnonymousPrefixNameByAdmin(
+    id: number,
+    body: AnonymousPropDto,
+  ): Promise<UpdateAnonymousProp>;
+  deleteAnonymousNameRuleByAdmin(id: number): Promise<DeleteAnonymousProp>;
+
+  findAllAnonymousPrefixRuleByAdmin(): Promise<FindAllAnonymousProp>;
+  findAllAnonymousNameRuleByAdmin(): Promise<FindAllAnonymousProp>;
 }
