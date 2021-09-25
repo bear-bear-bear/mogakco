43fcb94d596f478339eb59c93807cd3583f82078
Kim June Jae <bear-bear-bear@kakao.com>
2021-09-26 03:29:29 +0900

    feat: 채팅 추가 시 스크롤 bottom으로 가도록 추가 (#361)
    * feat: 채팅 입력시 스크롤이 제일 밑으로 가도록 설정
    
    * feat: 채팅 상단부를 되돌아보려고 스크롤한 경우엔 채팅이 추가되어도 스크롤 바텀으로 가지 않도록 설정

--- next/src/components/video-chat/ChatSection/ChatList/index.tsx
+++ next/src/components/video-chat/ChatSection/ChatList/index.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  useRef,
+} from 'react';
 import dynamic from 'next/dynamic';
 import { ViewerProps } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor-viewer.css';
@@ -22,19 +28,34 @@ export const MdViewerContext = createContext(Viewer);
 const returnVoid = () => undefined;
 
 const ChatList = () => {
-  const [message, setMessage] = useState<Message[]>([]);
+  const [chatList, setChatList] = useState<Message[]>([]);
   const client = useContext(SocketContext);
+  const chatListRef = useRef<HTMLUListElement>(null);
 
   useEffect(() => {
     if (!client) return returnVoid;
 
-    const addMessage = (info: Message) => {
-      setMessage((prevState) => prevState.concat(info));
+    const scrollToBottom = () => {
+      if (!chatListRef.current) return;
+
+      const chatListEl = chatListRef.current;
+      const { scrollHeight, clientHeight, scrollTop } = chatListEl;
+
+      const scrollTopMaxPixel = scrollHeight - clientHeight;
+      const isScrollAtNearlyEnd = scrollTop > scrollTopMaxPixel - clientHeight;
+      if (!isScrollAtNearlyEnd) return;
+
+      chatListEl.scrollTop = scrollTopMaxPixel;
     };
 
-    client.on('chat', addMessage);
-    client.on('exit', addMessage);
-    client.on('enter', addMessage);
+    const handleAddMessage = (newMessage: Message) => {
+      setChatList((prev) => [...prev, newMessage]);
+      scrollToBottom();
+    };
+
+    client.on('chat', handleAddMessage);
+    client.on('exit', handleAddMessage);
+    client.on('enter', handleAddMessage);
 
     return () => {
       client.off('chat');
@@ -45,8 +66,8 @@ const ChatList = () => {
 
   return (
     <MdViewerContext.Provider value={Viewer}>
-      <S.ChatList>
-        {message?.map((chat) => (
+      <S.ChatList ref={chatListRef}>
+        {chatList?.map((chat) => (
           <Chat key={chat.id} {...chat} />
         ))}
       </S.ChatList>
