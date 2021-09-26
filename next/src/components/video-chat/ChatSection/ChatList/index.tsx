df40c553b25586f50162d7f5f50e01671cf1383f
최은기 <galaxyhi4276@gmail.com>
2021-09-27 00:31:19 +0900

    feat: 채팅 사이드 이펙트 제거, 리렌더링 없앰

--- next/src/components/video-chat/ChatSection/ChatList/index.tsx
+++ next/src/components/video-chat/ChatSection/ChatList/index.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, useEffect, useState, useRef } from 'react';
+import React, {
+  createContext,
+  useEffect,
+  useState,
+  useRef,
+  useCallback,
+} from 'react';
 import dynamic from 'next/dynamic';
 import { ViewerProps } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor-viewer.css';
@@ -7,10 +13,11 @@ import useChatClient from '@hooks/chat/useChatClient';
 import { ChatAnnouncement, ChatMessage } from 'typings/chat';
 
 import { ChatEvent } from '@lib/enum';
+import useChatEvent from '@hooks/chat/useChatEvent';
 import Chat from '../Chat';
 import * as S from './style';
 
-type Message = ChatMessage | ChatAnnouncement;
+export type Message = ChatMessage | ChatAnnouncement;
 
 const Viewer = dynamic<ViewerProps>(
   () => import('@toast-ui/react-editor').then((m) => m.Viewer),
@@ -25,35 +32,28 @@ const ChatList = () => {
   const socketClient = useChatClient();
   const chatListRef = useRef<HTMLUListElement>(null);
 
-  useEffect(() => {
-    const scrollToBottom = () => {
-      if (!chatListRef.current) return;
+  const scrollToBottom = useCallback(() => {
+    if (!chatListRef.current) return;
 
-      const chatListEl = chatListRef.current;
-      const { scrollHeight, clientHeight, scrollTop } = chatListEl;
+    const chatListEl = chatListRef.current;
+    const { scrollHeight, clientHeight, scrollTop } = chatListEl;
 
-      const scrollTopMaxPixel = scrollHeight - clientHeight;
-      const isScrollAtNearlyEnd = scrollTop > scrollTopMaxPixel - clientHeight;
-      if (!isScrollAtNearlyEnd) return;
+    const scrollTopMaxPixel = scrollHeight - clientHeight;
+    const isScrollAtNearlyEnd = scrollTop > scrollTopMaxPixel - clientHeight;
+    if (!isScrollAtNearlyEnd) return;
 
-      chatListEl.scrollTop = scrollTopMaxPixel;
-    };
+    chatListEl.scrollTop = scrollTopMaxPixel;
+  }, []);
 
-    const handleAddMessage = (newMessage: Message) => {
+  const handleAddMessage = useCallback(
+    (newMessage: Message) => {
       setChatList((prev) => [...prev, newMessage]);
       scrollToBottom();
-    };
+    },
+    [scrollToBottom],
+  );
 
-    socketClient.on(ChatEvent.SEND_CHAT, handleAddMessage);
-    socketClient.on(ChatEvent.ROOM_EXIT, handleAddMessage);
-    socketClient.on(ChatEvent.ROOM_ENTER, handleAddMessage);
-
-    return () => {
-      socketClient.off(ChatEvent.SEND_CHAT);
-      socketClient.off(ChatEvent.ROOM_EXIT);
-      socketClient.off(ChatEvent.ROOM_ENTER);
-    };
-  }, [socketClient]);
+  useChatEvent(socketClient, handleAddMessage);
 
   return (
     <MdViewerContext.Provider value={Viewer}>
