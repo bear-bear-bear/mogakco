d3713376ba2bf010a335f5ddb06570b781318002
Kim June Jae <bear-bear-bear@kakao.com>
2021-09-30 04:16:01 +0900

    feat: FileChat 삭제 (#371)

--- next/typings/chat.ts
+++ next/typings/chat.ts
@@ -12,19 +12,10 @@ export interface ChatAnnouncement {
 }
 
 export interface ChatMessage {
-  type: 'chat';
   id: number;
   ownerId: number;
   username: string;
   message: string;
-}
-
-export interface ChatFile {
-  type: 'file';
-  id: number;
-  ownerId: number;
-  username: string;
-  info: { filename: string; size: number; url: string };
 }
 
 export interface UploadImageResponse extends IGeneralServerResponse {
