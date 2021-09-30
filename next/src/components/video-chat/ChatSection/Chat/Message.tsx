c2205966fd48c1a2e317770778180b1e19a3cf91
bearbearbear <ghlwkdthfl@gmail.com>
2021-10-01 03:16:38 +0900

    feat: 링크 preview 기능 추가

--- next/src/components/video-chat/ChatSection/Chat/Message.tsx
+++ next/src/components/video-chat/ChatSection/Chat/Message.tsx
@@ -1,4 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useCallback, useContext } from 'react';
+import _ from 'lodash';
+import * as linkify from 'linkifyjs';
+import { LinkPreview } from '@dhaiwat10/react-link-preview';
+import type { LinkPreviewProps } from '@dhaiwat10/react-link-preview';
+
 import useUser from '@hooks/useUser';
 import type { ChatMessage } from 'typings/chat';
 
@@ -9,10 +14,38 @@ const Message = ({ username, message, ownerId }: ChatMessage) => {
   const { user } = useUser();
   const MdViewer = useContext(MdViewerContext);
 
+  const getUniqueLinks = useCallback((plainText: string) => {
+    const hasProtocol = (url: string) => /(?:https?:\/\/).+/.test(url);
+
+    const linksWithProtocol = linkify
+      .find(plainText)
+      .filter(({ type, value }) => type === 'url' && hasProtocol(value));
+    const uniqueLinks = _.uniqBy(linksWithProtocol, ({ value }) =>
+      value.replace(/^www\./, ''),
+    );
+
+    return uniqueLinks.map(({ href }) => href);
+  }, []);
+
   return (
     <S.MessageWrapper>
       <S.Writer isMyChat={ownerId === user?.id}>{username}</S.Writer>
       <MdViewer initialValue={message} extendedAutolinks />
+      <ul>
+        {getUniqueLinks(message).map((link) => (
+          <li key={link}>
+            <LinkPreview
+              url={link}
+              width="100%"
+              imageHeight="9rem"
+              margin="0 0 0.66rem 0"
+              descriptionLength={100}
+              primaryTextColor="#21262d"
+              secondaryTextColor="#6e7681"
+            />
+          </li>
+        ))}
+      </ul>
     </S.MessageWrapper>
   );
 };
