c6996dd16234455b9a0c0963b3d657eb66175746
bearbearbear <ghlwkdthfl@gmail.com>
2021-10-05 02:56:59 +0900

    feat: AnyObject 타입을 공용 타입 모듈로 이동, KeyOf 타입 추가

--- next/typings/common.ts
+++ next/typings/common.ts
@@ -15,8 +15,18 @@ export interface IGeneralServerResponse {
 export type GeneralAxiosError = AxiosError<IGeneralServerResponse>;
 
 /**
+ * object 타입 대신 사용
+ */
+export type AnyObject = Record<string, any>;
+
+/**
  * T 에서 null 또는 undefined를 삭제한 필드 반환
  */
 export type NoUndefinedField<T> = {
   [P in keyof T]-?: Exclude<T[P], null | undefined>;
 };
+
+/**
+ * keyof 의 반환값인 string | number 에서 string 만을 반환해줌
+ */
+export type KeyOf<T extends AnyObject> = Extract<keyof T, string>;
