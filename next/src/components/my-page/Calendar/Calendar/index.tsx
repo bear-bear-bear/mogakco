b9c073c756b51788f62b734efa657c84256a3eb8
bearbearbear <ghlwkdthfl@gmail.com>
2021-10-21 23:58:01 +0900

    feat: 커스텀 스타일 임시 추가

--- next/src/components/my-page/Calendar/Calendar/index.tsx
+++ next/src/components/my-page/Calendar/Calendar/index.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { Calendar, Views, momentLocalizer } from 'react-big-calendar';
+import type { Event as IEvent, EventProps } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-// import '!style-loader!css-loader!react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment';
 import 'moment/locale/ko';
+
+import { theme } from '@globalStyles/theme';
 
 import events from './tempEvents';
 import * as S from './style';
@@ -11,20 +13,40 @@ import * as S from './style';
 const allViews = Object.values(Views);
 const localizer = momentLocalizer(moment);
 
-const Basic = () => {
-  return (
-    <S.CalendarWrapper>
-      <Calendar
-        events={events}
-        // views={allViews}
-        step={60}
-        showMultiDayTimes
-        // max={}
-        defaultDate={new Date()}
-        localizer={localizer}
-      />
-    </S.CalendarWrapper>
-  );
-};
+interface ICustomEvent extends IEvent {
+  desc?: string;
+}
 
-export default Basic;
+const Event = ({ event }: EventProps<ICustomEvent>) => (
+  <span>
+    <b>{event.title}</b>
+    {event.desc && `:  ${event.desc}`}
+  </span>
+);
+
+const EventAgenda = ({ event }: EventProps<ICustomEvent>) => (
+  <span>
+    <b style={{ color: theme.color['blue-1'] }}>{event.title}</b>
+    {event.desc && <p style={{ color: theme.color['gray-5'] }}>{event.desc}</p>}
+  </span>
+);
+
+const Rendering = () => (
+  <S.CalendarWrapper>
+    <Calendar<ICustomEvent>
+      views={allViews}
+      events={events}
+      localizer={localizer}
+      defaultDate={new Date()}
+      defaultView={Views.AGENDA}
+      components={{
+        event: Event,
+        agenda: {
+          event: EventAgenda,
+        },
+      }}
+    />
+  </S.CalendarWrapper>
+);
+
+export default Rendering;
--- next/src/components/my-page/Calendar/Calendar/style.tsx
+++ next/src/components/my-page/Calendar/Calendar/style.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 
 export const CalendarWrapper = styled.section`
+  max-width: 1280px;
   height: 800px;
   padding-bottom: 2rem;
 `;
