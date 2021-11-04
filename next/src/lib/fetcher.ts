d658992e4d9616e3c3064dc89cde3e9a09699160
bearbearbear <ghlwkdthfl@gmail.com>
2021-11-05 03:39:48 +0900

    feat: 유저 mock api 적용

--- next/src/lib/fetcher.ts
+++ next/src/lib/fetcher.ts
@@ -1,16 +1,20 @@
-// import axios from 'axios';
+import axios from 'axios';
 import apiClient, { logAxiosError } from '@lib/apiClient';
 import type { GeneralAxiosError } from 'typings/common';
 
 type ApiUrl = `/api/${string}`;
+
+const tempClient = axios.create({
+  withCredentials: true,
+});
 
 /**
  * @desc swr 요청에 사용되는 axios fetcher
  * @returns AxiosResponse의 data
  */
 export default async function fetcher(url: ApiUrl) {
-  const res = await apiClient.get<any>(url);
-  // const res = await axios.get<any>(url); // TODO: 유저를 Next Mock API 로 받기 위한 임시 fetcher (In useUser hooks)
+  // const res = await apiClient.get<any>(url);
+  const res = await tempClient.get<any>(url); // TODO: 유저를 Next Mock API 로 받기 위한 임시 fetcher (In useUser hooks)
 
   if (res.status >= 400) {
     logAxiosError(res.data as unknown as GeneralAxiosError);
