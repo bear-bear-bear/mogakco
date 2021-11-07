a653ee9338489a6b43334a38dbfae3d7ea79028d
bearbearbear <ghlwkdthfl@gmail.com>
2021-11-07 14:02:08 +0900

    temp: 작업 저장용 커밋

--- next/src/components/common/Todo/index.tsx
+++ next/src/components/common/Todo/index.tsx
@@ -1,9 +1,19 @@
+import useUser from '@hooks/useUser';
+import { IUserGetSuccessResponse } from 'typings/auth';
+
 import Container from './Container';
 import Header from './Header';
 import ColumnList from './ColumnList';
 import Column from './Column';
 
+// const SortContext =
+
 const Todo = () => {
+  const { user } = useUser();
+
+  if (!user?.isLoggedIn) return null;
+  const { todo: todoItems } = user as IUserGetSuccessResponse;
+
   return (
     <Container>
       <Header />
